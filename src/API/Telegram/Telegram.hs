b503dc9bb90315a1c9b9fab03cf2a4e22a235a47
KateBu <mayflower.studio@gmail.com>
2020-11-26 13:59:49 +0300

    new handle initialized, changed signatures

--- src/API/Telegram/Telegram.hs
+++ src/API/Telegram/Telegram.hs
@@ -15,11 +15,13 @@ import Data.Aeson
 
 import Config.Config 
 import qualified Logger.Logger as Logger 
+import qualified Logger.LoggerMsgs as LoggerMsgs
 import Handle.Handle 
 import API.Telegram.Structs 
 import API.Telegram.Parsers
 import Logic.PureStructs
-
+import Logic.Logic 
+import API.Telegram.Wrapper
 import qualified API.Telegram.Cleaners as Cleaners 
 
 
@@ -29,10 +31,7 @@ new config =  pure $ Handle
         hConfig = pure config 
         , hLogger = Logger.createLogger (priority config)
         , hGetUpdates = makeMessages config  
-        , hSendMessage = sendM config
-        , hSendMessage_ = sendM_ config
-        , hSetOffset = setOffset config 
-        , hEditUsers =undefined 
+        , hSendMessage_ = sendM_
     }
 
 close :: Handle m -> IO ()
@@ -47,45 +46,56 @@ withHandle config func params =
     bracket (new config) close (flip func params)
 
 
-getU :: Config -> IO (Either Err TelegramUpdates)
+getU :: Config -> IO (Either Logger.LogMessage TelegramUpdates)
 getU (TConfig _ off _ tok _ _) = do 
     http <- parseRequest $ "https://api.telegram.org/bot" <> tok <> "/getUpdates?offset=" <> show off
     updRequest <- httpLBS http
     let respBody = getResponseBody updRequest 
     decodeUpd respBody 
     
-makeMessages :: Config -> IO (Either Err [Message])
+makeMessages :: Config -> IO (Either Logger.LogMessage [Message])
 makeMessages config = do 
     updates <- getU config 
     Cleaners.updatesToPureMessageList updates 
 
+
+
     
 
-sendM :: Config -> Message -> IO (Maybe Config)
-sendM = undefined
+sendM :: Config -> [Message] -> IO (Either Logger.LogMessage Config)
+sendM config msgs= case msgs of 
+    [] -> return $ Right config 
+    _ -> undefined 
 
-sendM_ :: Config -> Message -> IO (Maybe ())
-sendM_ = undefined
+sendM_ :: Config -> Message -> IO (Either Logger.LogMessage Config)
+sendM_ config msg = do 
+    manager <- newManager tlsManagerSettings
+    let respObj =  makeMessageObject msg 
+    initialResponse <- parseRequest $ sendMessageHttpRequest config msg 
+
+    let resp = initialResponse {method = "POST"
+        , requestBody = RequestBodyLBS $ encode respObj
+        , requestHeaders = [("Content-Type", "application/json; charset=utf-8")]
+        }
+    response <- Network.HTTP.Client.httpLbs resp manager 
+    case statusCode (responseStatus response) of 
+        200 -> pure $ Right (configSetOffset config ((succ . getUid) msg )) 
+        err -> return $ Left (Logger.makeLogMessage LoggerMsgs.sndMsgFld ((T.pack . show) err))
+
+
 
 setOffset :: (Monad m) => Config -> Integer -> m Config 
 setOffset (TConfig msg _ rep tok us prior) newOffset = 
     pure $ (TConfig msg newOffset rep tok us prior)
 
 
-decodeUpd :: LC.ByteString -> IO (Either Err TelegramUpdates)
+decodeUpd :: LC.ByteString -> IO (Either Logger.LogMessage TelegramUpdates)
 decodeUpd js = case (decode js :: Maybe TelegramUpdates) of 
     Just val -> 
             return $ Right val
     Nothing -> case (eitherDecode js :: Either String TelegramUpdatesError) of 
-        Right val -> return $ Left ("\n\terror code: " <> (T.pack . show . error_code) val 
-            <> "\n\terror describtion: " <> description val)
-        Left msg -> return $ Left (T.pack msg)
+        Right val -> return $ Left (Logger.makeLogMessage LoggerMsgs.getUpdFld ("\n\terror code: " <> (T.pack . show . error_code) val 
+                <> "\n\terror describtion: " <> description val))
+        Left msg -> return $ Left (Logger.makeLogMessage LoggerMsgs.getUpdFld (T.pack msg))
             
     
-
-
-makeErrorMsg :: TelegramUpdatesError -> T.Text
-makeErrorMsg (TelegramUpdatesError code desc) = "error code: " 
-    <> T.pack (show code)
-    <> ", error describtion: "
-    <> desc 
