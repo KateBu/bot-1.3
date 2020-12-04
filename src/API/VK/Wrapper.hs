8223b8e6a17ef282b76da015391aa939a892194e
KateBu <mayflower.studio@gmail.com>
2020-12-04 08:40:50 +0300

    trying to make correct http-request, doesn't work correctly yet

--- src/API/VK/Wrapper.hs
+++ src/API/VK/Wrapper.hs
@@ -6,21 +6,44 @@ import Logic.PureStructs
 import Data.Aeson
 import Data.Aeson.Types
 import qualified Data.Text as T 
+import qualified Data.Text.Lazy as TL 
+import qualified Data.Text.Encoding as TLE 
+import Network.HTTP.Simple 
+
+--import qualified Data.ByteString as BC 
+import qualified Data.ByteString.Lazy as BL 
+import qualified Data.ByteString.Lazy.Char8 as BLC 
+--import qualified Data.ByteString.Char8 as 
+
 
 sendMessageHttpRequest :: Config -> String 
-sendMessageHttpRequest (Config (VK tok group _ _ _) _ _ _ _) = 
-    "https://api.vk.com/method/messages.send?group_id=" 
-    <> show group 
-    <> "access_token="
+sendMessageHttpRequest (Config (VK tok _ _ _ _) _ _ _ _) = 
+    "https://api.vk.com/method/messages.send?access_token="
     <> tok 
-
-makeMessageObject :: Message -> Value 
-makeMessageObject (CommonMessage _ chid msg _) = object $ 
-    ["user_id" .= chid]
+   -- <> "&v="
+   -- <> vkApiVersion
+  --  <> "&random_id="
+  --  <> show random_id 
+  --  <> "&user_id=10010924"
+  --  <> "&message=hello"
+{-
+makeMessageObject :: Message ->  Value 
+makeMessageObject (CommonMessage _ chid msg _)  = object $ 
+    ["user_id" .= show chid]
     <> ["peer_id" .= chid]
-    <> ["random_id" .= show 0]
+   -- <> ["random_id" .= (0 :: Integer)]
     <> messageToPairs msg 
+-}
 
+makeRequestBody :: Message -> Integer -> String
+makeRequestBody (CommonMessage _ chid msg _) randomId = mconcat
+    ["&v=", vkApiVersion
+    , "&user_id=", show chid
+    , "&random_id=", show randomId
+    , msgToBL msg]
+
+msgToBL :: CMessage -> String
+msgToBL (Txt txt)= T.unpack ("&message=" <> txt)
 
 
 messageToPairs :: CMessage -> [Pair]
@@ -35,3 +58,22 @@ getMaybeText _ _ = []
 getMaybeVal :: Show a => T.Text -> Maybe a -> [Pair]
 getMaybeVal prop (Just val) = [prop .= show val]
 getMaybeVal _ _ = [] 
+{-
+buildRequest :: BC.ByteString -> BC.ByteString -> BC.ByteString -> BC.ByteString -> Query -> Request 
+buildRequest token host method path  query = setRequestMethod method                         
+                        $ setRequestHost host 
+                        $ setRequestHeader "access_token" [token]
+                        $ setRequestPath path 
+                        $ setRequestQueryString query
+                        $ setRequestSecure True
+                        $ setRequestPort 443
+                        $ defaultRequest-}
+
+{-
+vkApiHost :: BC.ByteString
+vkApiHost = "api.vk.com"
+
+request :: Config -> Query -> Request 
+request ((VK tok group key serv ts) _ _ _ _) query = buildRequest tok vkApiHost "POST" "/method/messages.send" query 
+
+-}
