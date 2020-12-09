a88188b89210406f0eb420f7565a685e9b80ff2e
KateBu <mayflower.studio@gmail.com>
2020-12-09 15:19:36 +0300

    code formatted

--- src/Logger/LoggerMsgs.hs
+++ src/Logger/LoggerMsgs.hs
@@ -1,100 +1,128 @@
 module Logger.LoggerMsgs where
 
 import qualified Data.Text as T 
-import Logger.Logger 
+import qualified Logger.Logger as Logger 
 
 fatalConfig :: T.Text
 fatalConfig = "FATAL ERROR: couldn't find or read the config file," 
             <> " check the path to the file and the information inside it"
 
-parseVKConfFld :: LogMessage
-parseVKConfFld = LogMessage Error "Parse VK Config Failed: "
+parseVKConfFld :: Logger.LogMessage
+parseVKConfFld = Logger.LogMessage Logger.Error 
+    "Parse VK Config Failed: "
 
-parseVKConfNoInfo :: LogMessage
-parseVKConfNoInfo = LogMessage Error "Parse VK Config Failed: no information"
+parseVKConfNoInfo :: Logger.LogMessage
+parseVKConfNoInfo = Logger.LogMessage Logger.Error 
+    "Parse VK Config Failed: no information"
 
-emptyList :: LogMessage
-emptyList = LogMessage Debug "Empty list: nothing to process"
+emptyList :: Logger.LogMessage
+emptyList = Logger.LogMessage Logger.Debug 
+    "Empty list: nothing to process"
 
-emptyMsgScs :: LogMessage
-emptyMsgScs = LogMessage Debug "Empty message processed successfully!"
+emptyMsgScs :: Logger.LogMessage
+emptyMsgScs = Logger.LogMessage Logger.Debug 
+    "Empty message processed successfully!"
 
-emptyMsgFld :: LogMessage
-emptyMsgFld = LogMessage Error "Empty message process failed: "
+emptyMsgFld :: Logger.LogMessage
+emptyMsgFld = Logger.LogMessage Logger.Error 
+    "Empty message process failed: "
 
-cmdMsgFld :: LogMessage
-cmdMsgFld = LogMessage Error "Command message process failed: "
+cmdMsgFld :: Logger.LogMessage
+cmdMsgFld = Logger.LogMessage Logger.Error 
+    "Command message process failed: "
 
-comMsgScs :: LogMessage
-comMsgScs = LogMessage Debug "Common message processed successfully!"
+comMsgScs :: Logger.LogMessage
+comMsgScs = Logger.LogMessage Logger.Debug 
+    "Common message processed successfully!"
 
-comMsgFld :: LogMessage
-comMsgFld = LogMessage Error "Common message process failed: unknown command"
+comMsgFld :: Logger.LogMessage
+comMsgFld = Logger.LogMessage Logger.Error 
+    "Common message process failed: unknown command"
 
-hlpMsgScs :: LogMessage
-hlpMsgScs = LogMessage Debug "Help message processed successfully!"
+hlpMsgScs :: Logger.LogMessage
+hlpMsgScs = Logger.LogMessage Logger.Debug 
+    "Help message processed successfully!"
 
-hlpMsgFld :: LogMessage
-hlpMsgFld = LogMessage Error "Help message process failed: "
+hlpMsgFld :: Logger.LogMessage
+hlpMsgFld = Logger.LogMessage Logger.Error 
+    "Help message process failed: "
 
-rptMsgScs :: LogMessage
-rptMsgScs = LogMessage Debug "Repeat message processed successfully!"
+rptMsgScs :: Logger.LogMessage
+rptMsgScs = Logger.LogMessage Logger.Debug 
+    "Repeat message processed successfully!"
 
-rptMsgFld :: LogMessage
-rptMsgFld = LogMessage Error "Repeat message process failed: "
+rptMsgFld :: Logger.LogMessage
+rptMsgFld = Logger.LogMessage Logger.Error 
+    "Repeat message process failed: "
 
-getUpdScs :: LogMessage
-getUpdScs = LogMessage Debug "GetUpdates fuction processed successfully!"
+getUpdScs :: Logger.LogMessage
+getUpdScs = Logger.LogMessage Logger.Debug 
+    "GetUpdates fuction processed successfully!"
 
-getUpdFld :: LogMessage
-getUpdFld = LogMessage Error "GetUpdates function failed: "
+getUpdFld :: Logger.LogMessage
+getUpdFld = Logger.LogMessage Logger.Error 
+    "GetUpdates function failed: "
 
-prsMsgScs :: LogMessage
-prsMsgScs = LogMessage Debug "parseMessage function processed successfully!"
+prsMsgScs :: Logger.LogMessage
+prsMsgScs = Logger.LogMessage Logger.Debug 
+    "parseMessage function processed successfully!"
 
-prsMsgFld :: LogMessage
-prsMsgFld = LogMessage Error "parseMessage function failed: "
+prsMsgFld :: Logger.LogMessage
+prsMsgFld = Logger.LogMessage Logger.Error 
+    "parseMessage function failed: "
 
-sndMsgScs :: LogMessage
-sndMsgScs = LogMessage Debug "sendMessage function processed successfully!"
+sndMsgScs :: Logger.LogMessage
+sndMsgScs = Logger.LogMessage Logger.Debug 
+    "sendMessage function processed successfully!"
 
-sndMsgFld :: LogMessage
-sndMsgFld = LogMessage Error "sendMessage function failed: "
+sndMsgFld :: Logger.LogMessage
+sndMsgFld = Logger.LogMessage Logger.Error 
+    "sendMessage function failed: "
 
-prsConfScs :: LogMessage
-prsConfScs = LogMessage Debug "Config parsed successfully!"
+prsConfScs :: Logger.LogMessage
+prsConfScs = Logger.LogMessage Logger.Debug 
+    "Config parsed successfully!"
 
-prsConfFld :: LogMessage
-prsConfFld = LogMessage Error "Config parse failed: "
+prsConfFld :: Logger.LogMessage
+prsConfFld = Logger.LogMessage Logger.Error 
+    "Config parse failed: "
 
-noUpd :: LogMessage
-noUpd = LogMessage Debug "No updates to recieve"
+noUpd :: Logger.LogMessage
+noUpd = Logger.LogMessage Logger.Debug 
+    "No updates to recieve"
 
-chidNotFound :: LogMessage
-chidNotFound = LogMessage Error "Chat ID not found"
+chidNotFound :: Logger.LogMessage
+chidNotFound = Logger.LogMessage Logger.Error 
+    "Chat ID not found"
 
-nextLoop :: LogMessage
-nextLoop = LogMessage Debug "Go to the next loop"
+nextLoop :: Logger.LogMessage
+nextLoop = Logger.LogMessage Logger.Debug 
+    "Go to the next loop"
 
+vkUpdatesFailed1 :: Logger.LogMessage
+vkUpdatesFailed1 = Logger.LogMessage Logger.Warning 
+    "VKUpdate failed 1: Events was partly lost, use new ts value"
 
-vkUpdatesFailed1 :: LogMessage
-vkUpdatesFailed1 = LogMessage Warning "VKUpdate failed 1: Events was partly lost, use new ts value"
+vkUpdatesFailed2 :: Logger.LogMessage
+vkUpdatesFailed2 = Logger.LogMessage Logger.Warning 
+    "VKUpdate failed 2: Key value is not acceptable anymore, get new key value"
 
-vkUpdatesFailed2 :: LogMessage
-vkUpdatesFailed2 = LogMessage Warning "VKUpdate failed 2: Key value is not acceptable anymore, get new key value"
+vkUpdatesFailed3 :: Logger.LogMessage
+vkUpdatesFailed3 = Logger.LogMessage Logger.Warning 
+    "VKUpdate failed 3: Information was lost, get new key and ts values"
 
-vkUpdatesFailed3 :: LogMessage
-vkUpdatesFailed3 = LogMessage Warning "VKUpdate failed 3: Information was lost, get new key and ts values"
+vkUpdatesSuccess :: Logger.LogMessage
+vkUpdatesSuccess = Logger.LogMessage Logger.Debug 
+    "VK updates revieved successfully"
 
-vkUpdatesSuccess :: LogMessage
-vkUpdatesSuccess = LogMessage Debug "VK updates revieved successfully"
-
-vkUpdatesParsingUnknownMsgType :: LogMessage
-vkUpdatesParsingUnknownMsgType = LogMessage Error 
+vkUpdatesParsingUnknownMsgType :: Logger.LogMessage
+vkUpdatesParsingUnknownMsgType = Logger.LogMessage Logger.Error 
     "Cannot parse VK updates into Pure Message, unknonw VK updates event type"
 
-vkUpdatesParsingNoMsg :: LogMessage
-vkUpdatesParsingNoMsg = LogMessage Error "VK updates message not found"
+vkUpdatesParsingNoMsg :: Logger.LogMessage
+vkUpdatesParsingNoMsg = Logger.LogMessage Logger.Error 
+    "VK updates message not found"
 
-vkUpdatesParsingNoGroupID :: LogMessage
-vkUpdatesParsingNoGroupID = LogMessage Error "VK updates group ID not found"
+vkUpdatesParsingNoGroupID :: Logger.LogMessage
+vkUpdatesParsingNoGroupID = Logger.LogMessage Logger.Error 
+    "VK updates group ID not found"
