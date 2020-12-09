e6aa714d4fa82ef8f0ef61e2dd0086b7792d0864
KateBu <mayflower.studio@gmail.com>
2020-12-09 15:38:18 +0300

    code fromatted

--- src/API/VK/Parsers.hs
+++ src/API/VK/Parsers.hs
@@ -1,87 +1,86 @@
 module API.VK.Parsers where
 
-import API.VK.Structs
+import qualified API.VK.Structs as VKStructs
 
-import Data.Aeson 
+import Data.Aeson
+    ( FromJSON(parseJSON), Value(Object), withObject, (.:), (.:?) ) 
 
 
-instance FromJSON VKUpdates where 
+instance FromJSON VKStructs.VKUpdates where 
     parseJSON = withObject "VKUpdates" $ \obj -> do
         resp <- obj .:? "failed"
         case (resp :: Maybe Int) of 
-            Just val -> VKUpdateError val <$> 
+            Just val -> VKStructs.VKUpdateError val <$> 
                 obj .:? "ts"
-            Nothing -> VKUpdates <$> --obj .: "ts"
+            Nothing -> VKStructs.VKUpdates <$> 
                 obj .: "updates"
 
-
-{-
-
-instance FromJSON EventType where 
-    parseJSON = withObject "EventType" $ \obj -> do 
-        upType <- obj .: "type" 
-        case (upType :: String) of 
-            "message_new" -> pure MsgNew 
-            _ -> pure OtherEvent 
--}
-instance FromJSON VKMessage where 
+instance FromJSON VKStructs.VKMessage where 
     parseJSON (Object obj) = 
-        VKMessage <$> obj .: "id"
+        VKStructs.VKMessage <$> obj .: "id"
             <*> obj .: "peer_id"
             <*> obj .: "from_id"
             <*> obj .:? "text"
+            <*> obj .:? "attachments"
 
-instance FromJSON VKUpdInfo where 
+instance FromJSON VKStructs.Attachment where 
+    parseJSON = withObject "Attachment" $ \obj -> do 
+        attachType <- obj .: "type"
+        case (attachType :: String) of 
+            "photo" -> do
+                photo <- obj .: "photo" 
+                VKStructs.Attachment attachType <$> 
+                    (VKStructs.VKPhoto <$> photo .: "id"
+                    <*> photo .: "owner_id"
+                    <*> photo .: "text"
+                    <*> photo .: "access_key")
+            _ -> pure VKStructs.UnknownAttachment 
+
+instance FromJSON VKStructs.VKUpdInfo where 
     parseJSON = withObject "VKUpdInfo" $ \obj -> do 
         upType <- obj .: "type"
         case (upType :: String) of 
-            "message_new" -> VKUpdInfo MsgNew <$>
+            "message_new" -> VKStructs.VKUpdInfo VKStructs.MsgNew <$>
                 obj .:? "object"
                 <*> obj .:? "group_id"
-            _ -> pure $ VKUpdInfo OtherEvent Nothing Nothing 
+            _ -> pure $ VKStructs.VKUpdInfo VKStructs.OtherEvent Nothing Nothing 
 
-instance FromJSON VKObject where 
-    parseJSON (Object obj) = VKObject <$> obj .: "message"
+instance FromJSON VKStructs.VKObject where 
+    parseJSON (Object obj) = VKStructs.VKObject <$> obj .: "message"
         <*> obj .: "client_info"
 
-instance FromJSON ClientInfo where 
-    parseJSON (Object obj) = ClientInfo <$> obj .: "button_actions" 
+instance FromJSON VKStructs.ClientInfo where 
+    parseJSON (Object obj) = VKStructs.ClientInfo <$> obj .: "button_actions" 
         <*> obj .: "keyboard"
         <*> obj .: "inline_keyboard"
 
-
-
-instance FromJSON VKResult where 
+instance FromJSON VKStructs.VKResult where 
     parseJSON = withObject "VKResult" $ \obj -> do 
         isError <- obj .:? "error"
         case isError of 
-            Just val -> pure $ SendMsgError val 
-            Nothing -> SendMsgScs <$> obj .: "response"
+            Just val -> pure $ VKStructs.SendMsgError val 
+            Nothing -> VKStructs.SendMsgScs <$> obj .: "response"
 
-
-instance FromJSON VKResultError where 
-    parseJSON (Object obj) = VKResultError <$> 
+instance FromJSON VKStructs.VKResultError where 
+    parseJSON (Object obj) = VKStructs.VKResultError <$> 
         obj .: "error_code"
         <*> obj .: "error_msg"
 
-
-instance FromJSON VKResponse where 
+instance FromJSON VKStructs.VKResponse where 
     parseJSON = withObject "VKResponse" $ \obj -> do
         resp <- obj .:? "response"
         case resp of 
             Just val ->             
-                VKResponse <$> val .: "key"
+                VKStructs.VKResponse <$> val .: "key"
                 <*> val .: "server"
                 <*> val .: "ts"
             Nothing -> do 
                 err <- obj .:? "error"
                 case err of 
                     Just val -> 
-                        VKError <$> val .: "error_code"
+                        VKStructs.VKError <$> val .: "error_code"
                         <*> val .: "error_msg"
                     Nothing -> 
-                        pure VKParseError 
-
-
+                        pure VKStructs.VKParseError 
 
 
