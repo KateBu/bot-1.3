f70935d87cc7a0f2b2a20e96e4e4d271386b5faa
KateBu <mayflower.studio@gmail.com>
2020-12-10 13:17:20 +0300

    warnings heeded, pureStruct changes made

--- src/API/VK/Wrapper.hs
+++ src/API/VK/Wrapper.hs
@@ -1,48 +1,28 @@
 module API.VK.Wrapper where
 
-import Config.Config 
-import Logic.PureStructs 
-
-import Data.Aeson
-import Data.Aeson.Types
 import qualified Data.Text as T 
-import qualified Data.Text.Lazy as TL 
-import qualified Data.Text.Encoding as TLE 
-import Network.HTTP.Simple 
-import Network.HTTP.Client.Conduit
 
-import qualified Data.ByteString as BS 
-import qualified Data.ByteString.Lazy as BL 
-import qualified Data.ByteString.Lazy.Char8 as BLC 
-import qualified Data.ByteString.Char8 as BSC 
+import qualified Config.Config as Config 
+import qualified Logic.PureStructs as PureStructs 
 
 
-sendMessageHttpRequest :: Config -> String 
-sendMessageHttpRequest (Config (VK tok _ _ _ _) _ _ _ _) = 
+sendMessageHttpRequest :: Config.Config -> String 
+sendMessageHttpRequest (Config.Config (Config.VK tok _ _ _ _) _ _ _ _) = 
     "https://api.vk.com/method/messages.send?access_token="
     <> tok 
+sendMessageHttpRequest _ = ""
 
-
-makeRequestBody :: Message -> Int -> String
-makeRequestBody (CommonMessage _ chid msg _) randomId = mconcat
-    ["&v=", vkApiVersion
+makeRequestBody :: PureStructs.Message -> Int -> String
+makeRequestBody (PureStructs.CommonMessage _ chid msg _) randomId = mconcat
+    ["&v=", Config.vkApiVersion
     , "&user_id=", show chid
     , "&random_id=", show randomId
     , msgToBL msg]
+makeRequestBody _ _ = undefined 
 
-msgToBL :: CMessage -> String
-msgToBL (Txt txt)= T.unpack ("&message=" <> txt)
+msgToBL :: PureStructs.ComMessage -> String
+msgToBL cMsg = getMaybeText (PureStructs.mbText cMsg)
 
-
-messageToPairs :: CMessage -> [Pair]
-messageToPairs (Txt txt ) = ["message" .= txt]
-
-
-
-getMaybeText :: T.Text -> Maybe T.Text -> [Pair] 
-getMaybeText prop (Just val) = [prop .= val]
-getMaybeText _ _ = []
-
-getMaybeVal :: Show a => T.Text -> Maybe a -> [Pair]
-getMaybeVal prop (Just val) = [prop .= show val]
-getMaybeVal _ _ = [] 
+getMaybeText :: Maybe T.Text -> String
+getMaybeText Nothing = ""
+getMaybeText (Just txt) = T.unpack ("&message=" <> txt)
