552fca2e8c2c41af5a60194430ee96db11469e0b
KateBu <mayflower.studio@gmail.com>
2020-12-10 13:15:21 +0300

    warnings heeded, parsers moved here

--- src/API/Telegram/Structs.hs
+++ src/API/Telegram/Structs.hs
@@ -2,7 +2,19 @@ module API.Telegram.Structs where
 
 import qualified Data.Text as T 
 import GHC.Generics ( Generic )
+import Data.Aeson
+    ( (.:),
+      (.:?),
+      object,
+      FromJSON(parseJSON),
+      Value(Object),
+      KeyValue((.=)),
+      ToJSON(toJSON) ) 
+import Data.Aeson.Types (parseFail)
 
+
+parseFailMessage :: String
+parseFailMessage = "Telegram bot got unexpected imput data type while parsing JSON"
 
 data TelegramUpdatesError = TelegramUpdatesError 
     {
@@ -10,11 +22,15 @@ data TelegramUpdatesError = TelegramUpdatesError
         , description :: T.Text
     } deriving (Generic, Show)
 
+instance FromJSON TelegramUpdatesError
+
 data TelegramUpdates = TelegramUpdates 
     {
         ok :: Bool
         , result :: [TelUpdateResult]
     } deriving (Generic, Show)
+
+instance FromJSON TelegramUpdates
 
 data TelUpdateResult = TelUpdateResult 
     {
@@ -23,21 +39,44 @@ data TelUpdateResult = TelUpdateResult
         , callback_query :: Maybe Callback 
     } deriving Show 
 
+instance FromJSON TelUpdateResult where 
+    parseJSON (Object v) =
+        TelUpdateResult <$> v .: "update_id"
+        <*> v .:? "message"
+        <*> v .:? "callback_query"
+    parseJSON _ = parseFail parseFailMessage
+
 data Callback = Callback 
     {
         cb_msg :: CBMsg
         , cb_data :: T.Text
     } deriving Show 
 
+instance FromJSON Callback where 
+    parseJSON (Object v) = 
+        Callback <$> v .: "message"
+        <*> v .: "data"
+    parseJSON _ = parseFail parseFailMessage
+
 data CBMsg = CBMsg 
     {
         cb_chat :: CBChat
     } deriving Show 
 
+instance FromJSON CBMsg where 
+    parseJSON (Object v) = 
+        CBMsg <$> v .: "chat"
+    parseJSON _ = parseFail parseFailMessage
+
 data CBChat = CBChat
     {
         cb_chid :: Int 
     } deriving Show 
+
+instance FromJSON CBChat where 
+    parseJSON (Object v) = 
+        CBChat <$> v .: "id"
+    parseJSON _ = parseFail parseFailMessage
 
 data MessageInfo = MessageInfo 
     {
@@ -59,20 +98,37 @@ data MessageInfo = MessageInfo
         , sticker :: Maybe TelSticker
     } deriving Show 
 
+instance FromJSON MessageInfo where 
+    parseJSON (Object v) = 
+        MessageInfo <$> v .: "message_id"
+        <*> v .: "chat"
+        <*> v .:? "text"
+        <*> v .:? "animation"
+        <*> v .:? "audio"
+        <*> v .:? "document"
+        <*> v .:? "photo"
+        <*> v .:? "video"
+        <*> v .:? "voice"
+        <*> v .:? "contact"
+        <*> v .:? "poll"
+        <*> v .:? "venue"
+        <*> v .:? "location"
+        <*> v .:? "media_group_id"
+        <*> v .:? "caption" 
+        <*> v .:? "sticker"
+    parseJSON _ = parseFail parseFailMessage
+
 data TelChat = TelChat
     {
         chat_id :: Int
         ,chat_type :: T.Text 
     } deriving Show 
 
-data TelForward = TelForward 
-    {
-        f_id :: Maybe Int
-        , f_is_bot :: Maybe Bool
-        , f_first_name :: Maybe T.Text
-        , f_last_name :: Maybe T.Text
-        , f_username :: Maybe T.Text
-    } deriving Show 
+instance FromJSON TelChat where 
+    parseJSON (Object v) = 
+        TelChat <$> v .: "id"
+            <*> v .: "type"
+    parseJSON _ = parseFail parseFailMessage
 
 data TelSticker = TelSticker
     {
@@ -80,6 +136,12 @@ data TelSticker = TelSticker
         , s_is_animated :: Bool 
 
     } deriving Show 
+
+instance FromJSON TelSticker where 
+    parseJSON (Object v) = 
+        TelSticker <$> v .: "file_id"
+        <*> v .: "is_animated"
+    parseJSON _ = parseFail parseFailMessage
 
 data TelAudio = TelAudio 
     {
@@ -89,15 +151,33 @@ data TelAudio = TelAudio
         , audio_title :: Maybe T.Text
     } deriving Show
 
+instance FromJSON TelAudio where 
+    parseJSON (Object v) = 
+        TelAudio <$> v .: "file_id"
+        <*> v .:? "duration"
+        <*> v .:? "performer"
+        <*> v .:? "title"
+    parseJSON _ = parseFail parseFailMessage
+
 data TelPhoto = TelPhoto 
     {
         photo_file_id :: T.Text
     } deriving Show
 
+instance FromJSON TelPhoto where 
+    parseJSON (Object v) = 
+        TelPhoto <$> v .: "file_id"
+    parseJSON _ = parseFail parseFailMessage
+
 data TelDocument = TelDocument 
     {
          doc_file_id :: T.Text
     } deriving Show 
+
+instance FromJSON TelDocument where 
+    parseJSON (Object v) = 
+        TelDocument <$> v .: "file_id"
+    parseJSON _ = parseFail parseFailMessage
 
 data TelLocation = TelLocation
     {
@@ -105,20 +185,37 @@ data TelLocation = TelLocation
         , longitude :: Double 
     } deriving (Generic, Show ) 
 
+instance FromJSON TelLocation
+
 data TelVideo = TelVideo 
     {
         video_file_id :: T.Text
     } deriving Show 
+
+instance FromJSON TelVideo where 
+    parseJSON (Object v) = 
+        TelVideo <$> v .: "file_id"
+    parseJSON _ = parseFail parseFailMessage
 
 data TelVoice = TelVoice
     {
         voice_file_id :: T.Text
     } deriving Show 
 
+instance FromJSON TelVoice where 
+    parseJSON (Object v) = 
+        TelVoice <$> v .: "file_id"
+    parseJSON _ = parseFail parseFailMessage
+
 data TelAmination = TelAmination 
     {
         animation_file_id :: T.Text
     } deriving Show 
+
+instance FromJSON TelAmination where 
+    parseJSON (Object v) =
+        TelAmination <$> v .: "file_id"
+    parseJSON _ = parseFail parseFailMessage
 
 data TelContact = TelContact
     {
@@ -127,6 +224,14 @@ data TelContact = TelContact
         , last_name :: Maybe T.Text
         , vcard :: Maybe T.Text
     } deriving (Show)
+
+instance FromJSON TelContact where
+    parseJSON (Object v) = 
+        TelContact <$> v.: "phone_number"
+            <*> v .: "first_name"
+            <*> v .:? "last_name"
+            <*> v .:? "vcard"
+    parseJSON _ = parseFail parseFailMessage
 
 data TelPoll = TelPoll 
     {
@@ -142,11 +247,31 @@ data TelPoll = TelPoll
         , is_closed :: Maybe Bool 
     } deriving Show 
 
+instance FromJSON TelPoll where 
+    parseJSON (Object v) = 
+        TelPoll <$> v .: "question"
+        <*> v .: "options"
+        <*> v .:? "is_anonymous"
+        <*> v .:? "type"
+        <*> v .:? "allows_multiple_answers"
+        <*> v .:? "correct_option_id"
+        <*> v .:? "explanation"
+        <*> v .:? "open_period"
+        <*> v .:? "close_date"
+        <*> v .:? "is_closed"
+    parseJSON _ = parseFail parseFailMessage
+
 data PollOptions = PollOptions 
     {
         poll_option :: T.Text
         , voter_count :: Int
     } deriving Show 
+
+instance FromJSON PollOptions where 
+    parseJSON (Object v) = 
+        PollOptions <$> v .: "text"
+        <*> v .: "voter_count"
+    parseJSON _ = parseFail parseFailMessage
 
 data TelVenue = TelVenue
     {
@@ -156,8 +281,23 @@ data TelVenue = TelVenue
         , v_address :: T.Text
     } deriving Show 
 
+instance FromJSON TelVenue where 
+    parseJSON (Object v) = 
+        TelVenue <$> v .: "latitude"
+        <*> v .: "longitude"
+        <*> v .: "title"
+        <*> v .: "address"
+    parseJSON _ = parseFail parseFailMessage
+
 data Button = Button T.Text T.Text
     deriving Show 
 
+instance ToJSON Button where 
+    toJSON (Button btn cbd) = object ["text" .= btn, "callback_data" .= cbd] 
+
 data InlineKeyBoard = InlineKeyBoard [[Button]]
     deriving Show 
+
+instance ToJSON InlineKeyBoard where 
+    toJSON (InlineKeyBoard btns) = object ["inline_keyboard" .= btns]
+
