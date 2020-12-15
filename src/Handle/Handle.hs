41fe8646062bce7c9bc99005145cdf54f6996c48
KateBu <mayflower.studio@gmail.com>
2020-12-15 15:27:30 +0300

    son functions moved to this file

--- src/Handle/Handle.hs
+++ src/Handle/Handle.hs
@@ -1,14 +1,36 @@
 module Handle.Handle where
 
+import Control.Exception ( bracket )
+
 import qualified Config.Config as Config 
 import qualified Logger.Logger as Logger 
 import qualified Logic.PureStructs as PureStructs 
-
+import qualified API.Wrapper as Wrapper 
 
 data Handle m = Handle 
     {
-        hConfig :: m (Either Logger.LogMessage Config.Config)
+        hConfig :: m Config.Config
         , hLogger :: m Logger.Logger
-        , hGetUpdates :: Config.Config -> m (Either Logger.LogMessage [PureStructs.Message]) 
-        , hSendMessage_ :: Config.Config -> PureStructs.Message -> m (Either Logger.LogMessage Config.Config)
+        , hGetUpdates :: Config.Config -> Logger.Logger -> m (Either Logger.LogMessage [PureStructs.PureMessage]) 
+        , hSendMessage_ :: Config.Config -> Logger.Logger -> PureStructs.PureMessage -> m (Either Logger.LogMessage Config.Config)
     }
+
+new :: Config.Config -> IO (Handle IO) 
+new config = pure $ Handle 
+    {
+        hConfig = pure config 
+        , hLogger = Logger.createLogger (Config.priority config)
+        , hGetUpdates = Wrapper.getPureMessageList -- undefined -- makeMessages  
+        , hSendMessage_ = Wrapper.sendM-- undefined --sendM_
+    }
+
+close :: Handle m -> IO ()
+close _ = pure ()
+
+withHandleNoParams :: Config.Config -> (Handle IO -> IO a) -> IO a 
+withHandleNoParams config = 
+    bracket (new config) close 
+
+withHandle :: Config.Config -> (Handle IO -> c -> IO a) -> c -> IO a 
+withHandle config func params = 
+    bracket (new config) close (flip func params)
