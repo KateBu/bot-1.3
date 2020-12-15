c050b6a5e8af0c90e67ee6bc130f8d299184e2e2
KateBu <mayflower.studio@gmail.com>
2020-12-15 15:24:29 +0300

    specific telegram data added

---
+++ src/API/Telegram/TelData.hs
@@ -0,0 +1,22 @@
+module API.Telegram.TelData where
+
+import qualified Data.Text as T 
+
+import qualified Logic.PureStructs as PureStructs 
+import qualified Config.Config as Config 
+
+updateHostPath :: Config.BotType -> Maybe PureStructs.HostPath
+updateHostPath (Config.Telegram tok _) = pure $ PureStructs.HostPath "api.telegram.org" ["bot" <> tok,"getUpdates"]
+updateHostPath _ = Nothing 
+
+updateParams :: Config.BotType -> [PureStructs.Params]
+updateParams (Config.Telegram _ offset) = [PureStructs.ParamsNum "offset" offset
+    , PureStructs.ParamsText "timeout" Config.timeOut]
+updateParams _ = []
+
+sendHostPath :: Config.BotType -> PureStructs.MessageType -> Maybe PureStructs.HostPath
+sendHostPath (Config.Telegram tok _) (PureStructs.MTCommon mType) = pure $ 
+    PureStructs.HostPath "api.telegram.org" [
+        "bot"<> tok 
+        , "send" <> mType
+        ]
