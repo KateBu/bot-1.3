a086e47ad120cd5e0d5b3ad91c0a2c1b3634afb2
KateBu <mayflower.studio@gmail.com>
2020-12-16 10:16:11 +0300

    hostPath for different message type added

--- src/API/Telegram/TelData.hs
+++ src/API/Telegram/TelData.hs
@@ -15,6 +15,18 @@ updateParams (Config.Telegram _ offset) = [PureStructs.ParamsNum "offset" offset
 updateParams _ = []
 
 sendHostPath :: Config.BotType -> PureStructs.MessageType -> Maybe PureStructs.HostPath
+sendHostPath (Config.Telegram tok _) PureStructs.MTEmpty = undefined 
+sendHostPath (Config.Telegram tok _) (PureStructs.MTCallbackQuery txt) = undefined 
+sendHostPath (Config.Telegram tok _) (PureStructs.MTUserCommand PureStructs.Help) = pure $
+    PureStructs.HostPath "api.telegram.org" [
+        "bot"<> tok 
+        , "sendMessage"
+        ]
+sendHostPath (Config.Telegram tok _) (PureStructs.MTUserCommand PureStructs.Repeat) = pure $
+    PureStructs.HostPath "api.telegram.org" [
+        "bot"<> tok 
+        , "sendMessage"
+        ]
 sendHostPath (Config.Telegram tok _) (PureStructs.MTCommon mType) = pure $ 
     PureStructs.HostPath "api.telegram.org" [
         "bot"<> tok 
