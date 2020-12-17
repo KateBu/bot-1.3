e2a0657250e90bb9cf0ea4eda835a0275670ca15
KateBu <mayflower.studio@gmail.com>
2020-12-17 14:14:15 +0300

    parsers from TUpdate to PureStructs almost completed

--- src/API/Telegram/Cleaners.hs
+++ src/API/Telegram/Cleaners.hs
@@ -2,9 +2,14 @@ module API.Telegram.Cleaners where
 
 import qualified Data.Text as T 
 import qualified Data.ByteString.Lazy as BSL 
-import Data.Aeson ( decode, eitherDecode ) 
+import Data.Aeson
+    ( decode,
+      eitherDecode,
+      object,
+      Value,
+      KeyValue((.=)),
+      ToJSON(toJSON) )
 import Control.Applicative ( Alternative((<|>)) )
-
 import qualified API.Telegram.Structs as TStructs 
 import qualified Logic.PureStructs as PureStructs 
 import qualified Logger.Logger as Logger 
@@ -51,10 +56,10 @@ mbMakeCallbackPureMessage :: TStructs.TelUpdateResult
     -> PureStructs.UpdateID
     -> Maybe PureStructs.PureMessage
 mbMakeCallbackPureMessage res uid = case TStructs.callback_query res of 
-    Just (TStructs.Callback _ cbData) -> pure (PureStructs.PureMessage 
-            (PureStructs.MTCallbackQuery cbData)
+    Just (TStructs.Callback callback cbData) -> pure (PureStructs.PureMessage 
+        (PureStructs.MTCallbackQuery cbData)
             uid 
-            Nothing
+            (Just $ (TStructs.cb_chid . TStructs.cb_chat) callback)
             Nothing)
     _ -> Nothing
 
@@ -63,7 +68,7 @@ mbGetMessageInfo :: Config.Config
     -> PureStructs.UpdateID
     -> Maybe PureStructs.PureMessage
 mbGetMessageInfo config res uid = case TStructs.messageInfo res of 
-    Nothing -> Nothing
+    Nothing -> pure $ PureStructs.PureMessage PureStructs.MTEmpty uid Nothing Nothing 
     Just mInfo -> do 
         let chid = TStructs.chat_id $ TStructs.chat mInfo 
         case makePureMessage config uid chid mInfo of 
@@ -84,12 +89,13 @@ makePureMessage config uid chid mInfo = mbAnimation uid chid mInfo
     <|> mbContact uid chid mInfo 
     <|> mbVenue uid chid mInfo
     <|> mbLocation uid chid mInfo    
---    <|> mbSticker uid chid mInfo
+    <|> mbSticker uid chid mInfo
+    <|> mbPoll uid chid mInfo
     <|> mbTextMessage config uid chid mInfo
 
 mbAnimation, mbAudio, mbDoc, mbVideo
     , mbVoice, mbPhoto, mbContact, mbVenue 
-    , mbLocation, mbSticker :: PureStructs.UpdateID 
+    , mbLocation, mbSticker, mbPoll :: PureStructs.UpdateID 
     -> PureStructs.ChatID 
     -> TStructs.MessageInfo 
     -> Maybe PureStructs.PureMessage      
@@ -105,7 +111,15 @@ mbTextMessage config uid chid mInfo = case TStructs.txt mInfo of
             uid 
             (Just chid)
             (Just $ basicParams chid mInfo 
-                <> [PureStructs.ParamsText "text" (Config.helpMessage config)])        
+                <> [PureStructs.ParamsText "text" (Config.helpMessage config)])   
+        "/repeat" -> pure $ PureStructs.PureMessage 
+            (PureStructs.MTUserCommand PureStructs.Repeat)
+            uid 
+            (Just chid)
+            (Just $ basicParams chid mInfo 
+                <> [PureStructs.ParamsJSON "reply_markup" (makeKeyboard ((map (map TButtons)) PureStructs.buttons'))
+                , PureStructs.ParamsBool "one_time_keyboard" True])
+
         _ -> pure $ PureStructs.PureMessage 
             (PureStructs.MTCommon "Message")
             uid 
@@ -212,9 +226,28 @@ mbSticker uid chid mInfo = case TStructs.sticker mInfo of
         uid 
         (Just chid)
         (Just $ basicParams chid mInfo 
-        <> [PureStructs.ParamsText "sticker" ((T.pack . show) $ TStructs.s_file_id sticker)
+        <> [PureStructs.ParamsText "sticker" (TStructs.s_file_id sticker)
         ])
     Nothing -> Nothing 
+
+mbPoll uid chid mInfo = case TStructs.poll mInfo of 
+    Just poll -> pure $ PureStructs.PureMessage 
+        (PureStructs.MTCommon "Poll")
+        uid 
+        (Just chid)
+        (Just $ basicParams chid mInfo 
+        <> [PureStructs.ParamsText "question" (TStructs.question poll)        
+        , PureStructs.ParamsTextList "options" (map TStructs.poll_option (TStructs.poll_options poll))]
+        <> makeMaybeBoolParams "is_anonimous" (TStructs.is_anonymous poll)
+    --    <> makeMaybeTextParams "type" (TStructs.poll_type poll)
+        <> makeMaybeBoolParams "allows_multiple_answers" (TStructs.allows_multiple_answers poll)
+        <> makeMaybeNumParams "correct_option_id" (TStructs.correct_option_id poll)
+        <> makeMaybeTextParams "explanation" (TStructs.explanation poll)
+        <> makeMaybeNumParams "open_period" (TStructs.open_period poll)
+        <> makeMaybeNumParams "close_date" (TStructs.close_date poll)
+        <> makeMaybeBoolParams "is_closed" (TStructs.is_closed poll)
+        )
+    Nothing -> Nothing
 
 getPhotoParams :: [TStructs.TelPhoto] -> [PureStructs.Params]
 getPhotoParams [] = []
@@ -232,131 +265,18 @@ makeMaybeTextParams ::  T.Text -> Maybe T.Text -> [PureStructs.Params]
 makeMaybeTextParams _ Nothing = [] 
 makeMaybeTextParams key (Just val) = pure $ PureStructs.ParamsText key val
 
+makeMaybeBoolParams :: T.Text -> Maybe Bool -> [PureStructs.Params]
+makeMaybeBoolParams _ Nothing = []
+makeMaybeBoolParams key (Just val) = pure $ PureStructs.ParamsBool key val
 
--- The functions below will be removed soon 
+makeMaybeNumParams :: T.Text -> Maybe Int -> [PureStructs.Params]
+makeMaybeNumParams _ Nothing = []
+makeMaybeNumParams key (Just val) = pure $ PureStructs.ParamsNum key val
 
-{-
-updResultToComMessage ::  TStructs.MessageInfo -> PureStructs.ComMessage 
-updResultToComMessage mInfo = case TStructs.sticker mInfo of 
-    Just val -> PureStructs.defaultComMsg {
-            PureStructs.commonMsgType = "Sticker"
-            , PureStructs.mbSticker = Just (PureStructs.PureSticker 
-                (TStructs.s_file_id val) 
-                (TStructs.s_is_animated val))
-        }
-    _ -> case TStructs.txt mInfo of 
-        Just val -> PureStructs.defaultComMsg {
-            PureStructs.commonMsgType = "Message"
-            , PureStructs.mbText = Just val
-        } 
-        _-> case TStructs.animation mInfo of 
-            Just val -> PureStructs.defaultComMsg {
-                PureStructs.commonMsgType = "Animation"
-                , PureStructs.mbAnimationFileId = Just (TStructs.animation_file_id val)
-            }   
-            _-> case TStructs.audio mInfo of 
-                Just val -> PureStructs.defaultComMsg {
-                    PureStructs.commonMsgType = "Audio"
-                    , PureStructs.mbAudio = Just (PureStructs.PureAudio
-                        (TStructs.audio_id val)
-                        (TStructs.audio_duration val)
-                        (TStructs.audio_performer val)
-                        (TStructs.audio_title val))
-                }  
-                _-> case TStructs.document mInfo of 
-                    Just val -> PureStructs.defaultComMsg {
-                        PureStructs.commonMsgType = "Document"
-                        , PureStructs.mbDocFileId = Just (TStructs.doc_file_id val)
-                    } 
-                    _-> case TStructs.photo mInfo of 
-                        Just val -> PureStructs.defaultComMsg {
-                            PureStructs.commonMsgType = "Photo"
-                            , PureStructs.mbPhotoFileIds = Just (TStructs.photo_file_id <$> val)
-                        } 
-                        _-> case TStructs.video mInfo of 
-                            Just val -> PureStructs.defaultComMsg {
-                                PureStructs.commonMsgType = "Video"
-                                , PureStructs.mbVideoFileId = Just (TStructs.video_file_id val)
-                            }
-                            _-> case TStructs.voice mInfo of 
-                                Just val -> PureStructs.defaultComMsg { 
-                                    PureStructs.commonMsgType = "Voice"
-                                    , PureStructs.mbVoiceFileId = Just (TStructs.voice_file_id val)
-                                } 
-                                _-> case TStructs.contact mInfo of 
-                                    Just val -> PureStructs.defaultComMsg {
-                                        PureStructs.commonMsgType = "Contact"
-                                        , PureStructs.mbContact = Just (PureStructs.PureContact 
-                                            (TStructs.phone_number val)
-                                            (TStructs.first_name val)
-                                            (TStructs.last_name val)
-                                            (TStructs.vcard val))
-                                    } 
-                                    _-> case TStructs.poll mInfo of 
-                                        Just val -> PureStructs.defaultComMsg {
-                                            PureStructs.commonMsgType = "Poll"
-                                            , PureStructs.mbPoll = Just (PureStructs.PurePoll 
-                                                (TStructs.question val)
-                                                (pollOptionToPair <$> (TStructs.poll_options val))
-                                                (TStructs.is_anonymous val)
-                                                (TStructs.poll_type val)
-                                                (TStructs.allows_multiple_answers val)
-                                                (TStructs.correct_option_id val)
-                                                (TStructs.explanation val)
-                                                (TStructs.open_period val)
-                                                (TStructs.close_date val)
-                                                (TStructs.is_closed val))
-                                        } 
-                                        _-> case TStructs.venue mInfo of 
-                                            Just val -> PureStructs.defaultComMsg {
-                                                PureStructs.commonMsgType = "Venue"
-                                                , PureStructs.mbVenue = Just (PureStructs.PureVenue
-                                                    (TStructs.v_latitude val)
-                                                    (TStructs.v_longitude val)
-                                                    (TStructs.v_title val)
-                                                    (TStructs.v_address val))
-                                            } 
-                                            _-> case TStructs.location mInfo of 
-                                                Just val -> PureStructs.defaultComMsg {
-                                                    PureStructs.commonMsgType = "Location"
-                                                    , PureStructs.mbLocation = Just (PureStructs.PureLocation 
-                                                        (TStructs.latitude val)
-                                                        (TStructs.longitude val))
-                                                } 
-                                                _-> PureStructs.defaultComMsg {
-                                                    PureStructs.commonMsgType = "Other"
-                                                }
+newtype TButtons = TButtons PureStructs.PureButtons 
 
-pollOptionToPair :: TStructs.PollOptions -> (T.Text,Int)
-pollOptionToPair (TStructs.PollOptions txt num) = (txt,num)
+instance ToJSON TButtons where 
+    toJSON (TButtons (PureStructs.PureButtons lbl txt)) = object ["text" .= lbl, "callback_data" .= txt] 
 
-updResultToMessage :: TStructs.TelUpdateResult -> IO PureStructs.Message
-updResultToMessage (TStructs.TelUpdateResult uid _ (Just callback)) = 
-    pure $ PureStructs.CallbackQuery 
-        uid 
-        ((TStructs.cb_chid . TStructs.cb_chat . TStructs.cb_msg) callback) 
-        (TStructs.cb_data callback)
-updResultToMessage (TStructs.TelUpdateResult uid Nothing _) = 
-    pure $ PureStructs.EmptyMessage uid 
-updResultToMessage (TStructs.TelUpdateResult uid (Just mInfo) _) = 
-    let msg = updResultToComMessage mInfo 
-    in case PureStructs.commonMsgType msg of 
-        "Message" -> case PureStructs.mbText msg of 
-            Just "/help" -> pure (PureStructs.UserCommand uid $ 
-                PureStructs.Command ((TStructs.chat_id . TStructs.chat) mInfo) "/help")
-            Just "/repeat" -> pure (PureStructs.UserCommand uid $ 
-                PureStructs.Command ((TStructs.chat_id . TStructs.chat) mInfo) "/repeat")
-            _ -> pure $ 
-                PureStructs.CommonMessage uid 
-                    ((TStructs.chat_id . TStructs.chat) mInfo) 
-                    msg  
-                    (TStructs.caption mInfo)                   
-        _ -> pure $ PureStructs.CommonMessage uid ((TStructs.chat_id . TStructs.chat) mInfo) msg (TStructs.caption mInfo)
-
-updatesToPureMessageList :: Either Logger.LogMessage TStructs.TelegramUpdates 
-    -> IO (Either Logger.LogMessage [PureStructs.Message])
-updatesToPureMessageList (Left err) = return $ Left err
-updatesToPureMessageList (Right tup) = do
-    msgs <- mapM updResultToMessage (TStructs.result tup)
-    return $ Right msgs 
-    -}
+makeKeyboard :: [[TButtons]] -> Value
+makeKeyboard btns = object ["inline_keyboard" .= btns]
