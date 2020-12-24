b98e4f2a75f68dc15ce710d278f014ac541182f9
KateBu <mayflower.studio@gmail.com>
2020-12-24 14:24:49 +0300

    unnecessary data removed

--- src/API/VK/VKData.hs
+++ src/API/VK/VKData.hs
@@ -4,22 +4,6 @@ import qualified Data.Text as T
 import System.Random ( newStdGen, Random(random) )
 import qualified Logic.PureStructs as PureStructs 
 import qualified Config.Config as Config 
-import qualified API.VK.Structs as VKStructs 
-
-urlToHostPath :: T.Text -> Maybe PureStructs.HostPath 
-urlToHostPath url = 
-    let 
-        hostPath = T.drop 3 (T.dropWhile (/= ':') $ T.takeWhile (/= '?') url)
-        (host,rest) = T.span ( /= '/') hostPath 
-        path = filter (/= "") (T.splitOn "/" rest)
-    in pure $ PureStructs.HostPath host path 
-
-urlToParams :: VKStructs.Url -> Maybe [PureStructs.Params] 
-urlToParams url = let 
-    params = T.drop 1 $ T.dropWhile (/='?') url 
-    keyVals = filter (/="") $ T.splitOn "&" params 
-    keys = T.splitOn "=" <$> keyVals   
-    in mapM listToParams keys  
 
 updateParams :: Config.BotType -> [PureStructs.Params]
 updateParams (Config.VKBot(Config.VK _ _ key _ ts)) = [PureStructs.ParamsText "act" "a_check"
@@ -30,12 +14,12 @@ updateParams (Config.VKBot(Config.VK _ _ key _ ts)) = [PureStructs.ParamsText "a
     ]
 updateParams _ = [] 
 
-updateHostPath :: Config.BotType -> Maybe PureStructs.HostPath 
-updateHostPath (Config.VKBot(Config.VK _ _ _ server _)) = pure $ let 
-    (host,rest) = T.span (/='/') (T.drop 8 server)
-    path = filter (/="") (T.splitOn "/" rest) 
+updateHostPath :: Config.BotType -> PureStructs.HostPath 
+updateHostPath (Config.VKBot(Config.VK _ _ _ server _)) = 
+    let (host,rest) = T.span (/='/') (T.drop 8 server)
+        path = filter (/="") (T.splitOn "/" rest) 
     in PureStructs.HostPath host path
-updateHostPath _ = Nothing 
+updateHostPath _ = PureStructs.HPEmpty  
 
 sendHostPath :: PureStructs.HostPath
 sendHostPath = PureStructs.HostPath "api.vk.com" ["method","messages.send"]
@@ -53,27 +37,3 @@ getRandonId :: IO Int
 getRandonId = do 
     gen <- newStdGen 
     pure $ ( (fst . random) gen :: Int)
-
-uploadPhotoServerHostPath :: PureStructs.HostPath
-uploadPhotoServerHostPath = PureStructs.HostPath "api.vk.com" ["method","photos.getMessagesUploadServer"]
-
-getUploadFileParams :: Config.BotType -> [PureStructs.Params]
-getUploadFileParams (Config.VKBot(Config.VK tok _ _ _ _)) = 
-    [
-        PureStructs.ParamsText "v" Config.vkApiVersion
-        , PureStructs.ParamsText "access_token" tok
-    ]
-getUploadFileParams _ = []
-
-uploadPhotoHostPath :: VKStructs.UploadUrl -> Maybe PureStructs.HostPath 
-uploadPhotoHostPath (VKStructs.UploadUrl url) = urlToHostPath url 
-    
-uploadPhotoParams :: VKStructs.UploadUrl -> Maybe [PureStructs.Params] 
-uploadPhotoParams (VKStructs.UploadUrl url) = urlToParams url 
-
-listToParams :: [T.Text] -> Maybe PureStructs.Params 
-listToParams (key : val : []) = Just $ PureStructs.ParamsText key val 
-listToParams _ = Nothing 
-
-savePhotoServerHostPath :: PureStructs.HostPath 
-savePhotoServerHostPath = PureStructs.HostPath "api.vk.com" ["method","photos.saveMessagesPhoto"]
