9be001994980c3390167f312de75bcb34a74b5cc
KateBu <mayflower.studio@gmail.com>
2020-12-24 13:44:54 +0300

    functions for vk photo processing were commented

--- src/API/Wrapper.hs
+++ src/API/Wrapper.hs
@@ -1,4 +1,3 @@
--- {-# LANGUAGE DataKinds #-}
 module API.Wrapper where
 
 import Network.HTTP.Req
@@ -7,6 +6,7 @@ import Network.HTTP.Req
       FormUrlEncodedParam,
       ReqBodyMultipart,
       ReqBodyUrlEnc(..),
+      NoReqBody(..),
       (/:),
       (=:),
       defaultHttpConfig,
@@ -24,7 +24,6 @@ import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BSL  
 import Data.Aeson ( eitherDecode, encode )
 import Control.Monad.IO.Class ( MonadIO(liftIO) )
-import Data.Maybe ( fromJust ) 
 import qualified Network.HTTP.Client.MultipartFormData as LM
 import qualified Config.Config as Config 
 import qualified Logger.Logger as Logger 
@@ -35,6 +34,25 @@ import qualified API.VK.Cleaners as VKCleaners
 import qualified API.VK.VKData as VKData  
 import qualified API.Telegram.TelData as TelData 
 import qualified API.Telegram.Cleaners as TelCleaners 
+
+urlToHostPath :: T.Text -> Maybe PureStructs.HostPath 
+urlToHostPath url = 
+    let 
+        hostPath = T.drop 3 (T.dropWhile (/= ':') $ T.takeWhile (/= '?') url)
+        (host,rest) = T.span ( /= '/') hostPath 
+        path = filter (/= "") (T.splitOn "/" rest)
+    in pure $ PureStructs.HostPath host path 
+
+urlToParams :: VKStructs.Url -> Maybe [PureStructs.Params] 
+urlToParams url = let 
+    params = T.drop 1 $ T.dropWhile (/='?') url 
+    keyVals = filter (/="") $ T.splitOn "&" params 
+    keys = T.splitOn "=" <$> keyVals   
+    in mapM listToParams keys  
+
+listToParams :: [T.Text] -> Maybe PureStructs.Params 
+listToParams (key : val : []) = Just $ PureStructs.ParamsText key val 
+listToParams _ = Nothing 
 
 hostPathToUrlScheme :: PureStructs.HostPath -> Url Https 
 hostPathToUrlScheme (PureStructs.HostPath hpHost hpPath) = urlScheme (https hpHost) hpPath 
@@ -58,9 +76,6 @@ paramToMultipart (PureStructs.ParamsDouble key val) = [LM.partLBS key (encode va
 paramToMultipart (PureStructs.ParamsBool key val) = [LM.partLBS key (encode val)]
 paramToMultipart (PureStructs.ParamsJSON key val) = [LM.partLBS key (encode val)]
 paramToMultipart (PureStructs.ParamsTextList key val) = [LM.partLBS key (encode val)]
-
---paramToLBS :: PureStructs.Params -> BSL.ByteString 
---paramToLBS = encode 
 
 paramsToUrlBody :: [PureStructs.Params] -> ReqBodyUrlEnc
 paramsToUrlBody params = ReqBodyUrlEnc $ mconcat (paramToUrl <$> params)
@@ -93,12 +108,60 @@ data Method = Update | Send deriving Show
 mkHostPath :: Config.Config -> Method -> Maybe PureStructs.PureMessage-> Url Https
 mkHostPath config Update _ = 
     case Config.botType config of     
-        vk@(Config.VKBot _)-> hostPathToUrlScheme (fromJust $ VKData.updateHostPath vk) 
-        tel@(Config.TBot _) -> hostPathToUrlScheme (fromJust $ TelData.updateHostPath tel) 
+        vk@(Config.VKBot _)-> hostPathToUrlScheme (VKData.updateHostPath vk) 
+        tel@(Config.TBot _) -> hostPathToUrlScheme (TelData.updateHostPath tel) 
 mkHostPath config Send (Just msg)= 
     case Config.botType config of 
         (Config.VKBot _)-> hostPathToUrlScheme VKData.sendHostPath 
-        tel@(Config.TBot _) -> hostPathToUrlScheme $ fromJust (TelData.sendHostPath tel (PureStructs.messageType msg)) 
+        tel@(Config.TBot _) -> hostPathToUrlScheme $ (TelData.sendHostPath tel (PureStructs.messageType msg)) 
+
+getResponseUrl :: Url Https
+    -> [PureStructs.Params]
+    -> Option Https  
+    -> IO (Either Logger.LogMessage LbsResponse) 
+getResponseUrl ulr params options = runReq defaultHttpConfig $ do     
+        response <- req 
+            POST
+            ulr
+            (paramsToUrlBody params)
+            lbsResponse 
+            options 
+        (pure . pure) response
+
+getResponseMultipart :: Url Https
+    -> [PureStructs.Params]
+    -> Option Https  
+    -> IO (Either Logger.LogMessage LbsResponse) 
+getResponseMultipart url params options = do 
+    multipartParams <- paramsToMultipartBody params
+    runReq defaultHttpConfig $ do      
+        response <- req 
+            POST
+            url
+            multipartParams
+            lbsResponse 
+            options 
+        (pure . pure) response
+
+getResponseNoBody :: Url Https
+    -> Option Https  
+    -> IO (Either Logger.LogMessage LbsResponse) 
+getResponseNoBody url options = do 
+    runReq defaultHttpConfig $ do      
+        response <- req 
+            POST
+            url
+            NoReqBody
+            lbsResponse 
+            options 
+        (pure . pure) response
+
+responseToLbsByteString :: Either Logger.LogMessage LbsResponse
+    -> IO (Either Logger.LogMessage BSL.ByteString) 
+responseToLbsByteString (Left err) = pure $ Left err 
+responseToLbsByteString (Right response) = case responseStatusCode response of 
+    200 -> pure $ Right (responseBody response :: BSL.ByteString)
+    err -> pure $ Left (Logger.makeLogMessage LoggerMsgs.badServerResponse ((T.pack . show) err))
 
 getPureMessageList :: Config.Config -> Logger.Logger -> IO (Either Logger.LogMessage [PureStructs.PureMessage])
 getPureMessageList config logger = getU config >>= byteStringToPureMessageList config logger 
@@ -106,38 +169,15 @@ getPureMessageList config logger = getU config >>= byteStringToPureMessageList c
 getU :: Config.Config ->  IO (Either Logger.LogMessage BSL.ByteString) 
 getU config = do
     let params = (updateParam (Config.botType config)) 
+    let url = mkHostPath config Update Nothing 
     if any isMultipart params 
-        then getUMultipartParams config params 
-        else getUUrlParams config params 
-
-getUUrlParams :: Config.Config -> [PureStructs.Params] -> IO (Either Logger.LogMessage BSL.ByteString) 
-getUUrlParams config params = runReq defaultHttpConfig $ do     
-        response <- req 
-            POST
-            (mkHostPath config Update Nothing)
-            (paramsToUrlBody params)
-            lbsResponse 
-            mempty        
-        (pure . pure) (responseBody response :: BSL.ByteString)  
-
-getUMultipartParams :: Config.Config -> [PureStructs.Params] -> IO (Either Logger.LogMessage BSL.ByteString) 
-getUMultipartParams config params = do 
-        multiPartParams <- paramsToMultipartBody params 
-        runReq defaultHttpConfig $ do     
-            response <- req 
-                POST
-                (mkHostPath config Update Nothing)
-                multiPartParams
-                lbsResponse 
-                mempty        
-            (pure . pure) (responseBody response :: BSL.ByteString)  
+        then getResponseMultipart url params mempty 
+            >>= responseToLbsByteString 
+        else getResponseUrl url params mempty 
+            >>= responseToLbsByteString   
 
 sendM :: Config.Config -> Logger.Logger -> PureStructs.PureMessage -> IO (Either Logger.LogMessage Config.Config) 
-sendM config logger basicMsg = do 
-    eiMsg <- mbUploadFile (Config.botType config) logger basicMsg 
-    case eiMsg of 
-        Left err -> pure $ Left err 
-        Right msg -> case PureStructs.mbParams msg of 
+sendM config logger msg = case PureStructs.mbParams msg of 
             Nothing -> pure $ Right config 
             Just params -> do     
                 basicParams <- mbSendOption (Config.botType config)
@@ -148,9 +188,11 @@ getApiResponse :: Config.Config
     -> [PureStructs.Params] 
     -> PureStructs.PureMessage 
     -> IO (Either Logger.LogMessage LbsResponse)  
-getApiResponse config basicParams params msg = if any isMultipart params 
-    then sendMMultipartParams config msg basicParams params
-    else sendMUrlParams config msg basicParams params         
+getApiResponse config basicParams params msg = do 
+    let hostPath = mkHostPath config Send (Just msg) 
+    if any isMultipart params 
+    then getResponseMultipart hostPath params basicParams  
+    else getResponseUrl hostPath params basicParams         
     
 checkApiResponse :: Config.Config
     -> Logger.Logger 
@@ -174,37 +216,6 @@ checkApiResponse config logger msg (Right lbsResp) = case responseStatusCode lbs
             pure $ Right (Config.configSetOffset config (succ (PureStructs.updateID msg))) 
     err -> return $ Left (Logger.makeLogMessage LoggerMsgs.sndMsgFld ((T.pack . show) err))
 
-sendMMultipartParams :: Config.Config 
-    -> PureStructs.PureMessage
-    -> Option Https
-    -> [PureStructs.Params] 
-    -> IO (Either Logger.LogMessage LbsResponse)  
-sendMMultipartParams config msg basic params = do
-    multiPartParams <- paramsToMultipartBody params 
-    response <- runReq defaultHttpConfig $ do 
-        req 
-            POST 
-            (mkHostPath config Send (Just msg))
-            multiPartParams
-            lbsResponse $
-            basic 
-    (pure . pure) response 
-            
-sendMUrlParams :: Config.Config 
-    -> PureStructs.PureMessage
-    -> Option Https
-    -> [PureStructs.Params] 
-    -> IO (Either Logger.LogMessage LbsResponse)  
-sendMUrlParams config msg basic params = do
-    response <- runReq defaultHttpConfig $ do 
-        req 
-            POST 
-            (mkHostPath config Send (Just msg))
-            (paramsToUrlBody params)
-            lbsResponse $
-            basic 
-    (pure . pure) response 
-
 byteStringToPureMessageList :: Config.Config -> Logger.Logger 
     -> Either Logger.LogMessage BSL.ByteString 
     -> IO (Either Logger.LogMessage [PureStructs.PureMessage])
@@ -213,7 +224,18 @@ byteStringToPureMessageList config@(Config.Config (Config.VKBot _)_ _ _ _) logge
 byteStringToPureMessageList config@(Config.Config (Config.TBot _)_ _ _ _) logger eiBS = 
     TelCleaners.telByteStringToPureMessageList config logger eiBS 
 
-mbUploadFile :: Config.BotType 
+{-
+
+
+isAttachmentParam :: PureStructs.Params -> Bool 
+isAttachmentParam (PureStructs.ParamsText "attachment" _) = True 
+isAttachmentParam _ = False 
+
+isVKPhotoParam ::  PureStructs.Params -> Bool 
+isVKPhotoParam (PureStructs.ParamsText "VKPhotoUrl" _) = True 
+isVKPhotoParam _ = False 
+
+mbUploadFile :: Config.BotType
     -> Logger.Logger
     -> PureStructs.PureMessage 
     -> IO (Either Logger.LogMessage PureStructs.PureMessage)
@@ -224,12 +246,196 @@ mbUploadFile botType logger msg = case PureStructs.messageType msg of
             else pure $ Right msg
     _ -> pure $ Right msg 
 
-isVKPhotoParam ::  PureStructs.Params -> Bool 
-isVKPhotoParam (PureStructs.ParamsText "VKPhotoUrl" _) = True 
-isVKPhotoParam _ = False 
-
-makeUploadFileParams :: Config.BotType
+makeUploadFileParams :: Config.BotType 
     -> Logger.Logger 
     -> PureStructs.PureMessage 
     -> IO (Either Logger.LogMessage PureStructs.PureMessage)
-makeUploadFileParams = undefined
+makeUploadFileParams botType logger msg = do 
+    Logger.botLog logger LoggerMsgs.needUpload 
+    eiParams <- makeParams botType logger (PureStructs.mbParams msg) 
+    case eiParams of 
+        Left err -> pure $ Left err 
+        Right params -> pure $ Right msg {PureStructs.mbParams = params}
+
+makeParams :: Config.BotType 
+    -> Logger.Logger 
+    -> Maybe [PureStructs.Params] 
+    -> IO (Either Logger.LogMessage (Maybe [PureStructs.Params]))
+makeParams _ _ Nothing = pure $ Left LoggerMsgs.noParams
+makeParams botType logger (Just params) = do 
+    let atParams = filter isAttachmentParam params 
+    let phParams = filter isVKPhotoParam params 
+    let otherParams = filter (not . isAttachmentParam) $ filter (not . isVKPhotoParam) params 
+    eiNewParams <- makeAttachmentParams botType logger atParams phParams
+    case eiNewParams of 
+        Left err -> pure $ Left err 
+        Right newParams -> pure $ Right (Just otherParams <> Just newParams)
+
+makeAttachmentParams :: Config.BotType 
+    -> Logger.Logger 
+    -> [PureStructs.Params] 
+    -> [PureStructs.Params] 
+    -> IO (Either Logger.LogMessage [PureStructs.Params])
+makeAttachmentParams botType logger atParam phParam = do 
+    Logger.botLog logger LoggerMsgs.mkNewParam 
+    eiPhotoInfo <- setUploadedPhotoInfo botType logger phParam 
+    case eiPhotoInfo of 
+        Left err -> pure $ Left err 
+        Right phInfo -> case atParam of 
+            [] -> pure $ Right [PureStructs.ParamsText "attachment" phInfo ]
+            [PureStructs.ParamsText "attachment" attachments] -> pure $ 
+                Right [PureStructs.ParamsText "attachment" (attachments <> "," <> phInfo)]
+            _ -> pure $ Left LoggerMsgs.unexpAttachment 
+
+setUploadedPhotoInfo :: Config.BotType 
+    -> Logger.Logger 
+    -> [PureStructs.Params] 
+    -> IO (Either Logger.LogMessage T.Text)
+setUploadedPhotoInfo botType logger params = do 
+    eiPhotoParams <- getPhotosInfo botType logger params 
+    case eiPhotoParams of 
+        Left err -> pure $ Left err 
+        Right phParams -> pure $ Right (T.intercalate "," phParams)
+
+getPhotosInfo :: Config.BotType 
+    -> Logger.Logger 
+    -> [PureStructs.Params] 
+    -> IO (Either Logger.LogMessage [T.Text])
+getPhotosInfo botType logger params = do 
+    eiPhotoInfo <- mapM (getPhotoInfo botType logger) params 
+    case sequence eiPhotoInfo of 
+        Left err -> pure $ Left err 
+        Right phInfo -> pure $ Right phInfo 
+
+getPhotoInfo :: Config.BotType 
+    -> Logger.Logger 
+    -> PureStructs.Params 
+    -> IO (Either Logger.LogMessage T.Text)
+getPhotoInfo botType logger param = do 
+    Logger.botLog logger LoggerMsgs.getPhotoInfoIsRunning
+    param <- makePhotoInfo botType logger param 
+    case param of 
+        Left err -> pure $ Left err 
+        Right (VKStructs.VKPhoto itemId ownerId _) -> 
+            pure $ Right ("photo" 
+                <> (T.pack . show) ownerId 
+                <> "_"
+                <> (T.pack . show) itemId)
+        _ -> pure $ Left LoggerMsgs.unexpObject 
+
+makePhotoInfo :: Config.BotType 
+    -> Logger.Logger 
+    -> PureStructs.Params 
+    -> IO (Either Logger.LogMessage VKStructs.AObject)
+makePhotoInfo botType logger (PureStructs.ParamsText "VKPhotoUrl" url) = undefined 
+
+downloadedPhotoParams :: Logger.Logger -> T.Text -> IO (Either Logger.LogMessage [PureStructs.Params])
+downloadedPhotoParams logger url = do 
+    Logger.botLog logger LoggerMsgs.mkPhInfo 
+    downloadPhotoLbs url >>= photoLbsToParam 
+
+getUploadServerHostPathParams :: Config.BotType 
+    -> Logger.Logger
+    -> IO (Either Logger.LogMessage (PureStructs.HostPath, [PureStructs.Params]))
+getUploadServerHostPathParams botType logger = getServerToUploadUrl botType logger
+    >>= makeVKUploadPhotoServerResponse logger
+    >>= uploadServerHostPathParams logger 
+
+uploadPhotoToServer :: Config.BotType
+    -> Logger.Logger 
+    -> T.Text
+    -> IO (Either Logger.LogMessage BSL.ByteString)  
+uploadPhotoToServer botType logger url = do 
+    photoParams <- downloadedPhotoParams logger url 
+    server <- getUploadServerHostPathParams botType logger 
+    tryToUpload logger server photoParams >>= responseToLbsByteString 
+
+tryToUpload :: Logger.Logger 
+    -> Either Logger.LogMessage (PureStructs.HostPath, [PureStructs.Params])
+    -> Either Logger.LogMessage [PureStructs.Params] 
+    -> IO (Either Logger.LogMessage LbsResponse)
+tryToUpload _ (Left err) _ = pure $ Left err 
+tryToUpload _ _ (Left err) = pure $ Left err 
+
+
+
+{-
+getUploadServerHostPathParams :: Config.BotType 
+    -> IO (Either Logger.LogMessage (PureStructs.HostPath, [PureStructs.Params]))
+getUploadServerHostPathParams botType = getUploadServer botType 
+    >>= makeVKUploadPhotoServerResponse 
+    >>= uploadServerHostPathParams
+
+uploadServerHostPathParams :: Either Logger.LogMessage VKStructs.VKUploadPhotoServerResponse
+    -> IO (Either Logger.LogMessage (PureStructs.HostPath, [PureStructs.Params])) 
+uploadServerHostPathParams (Left err) = pure $ Left err 
+uploadServerHostPathParams (Right (VKStructs.PhotoServerError _)) = pure $ Left LoggerMsgs.noHostPath 
+uploadServerHostPathParams (Right (VKStructs.PhotoServer (VKStructs.UploadUrl url))) = 
+    case Wrapper.urlToHostPath url of 
+        Nothing -> pure $ Left LoggerMsgs.noHostPath 
+        Just hostPath -> case Wrapper.urlToParams url of 
+            Nothing -> pure $ Left LoggerMsgs.noParams 
+            Just params -> pure $ Right (hostPath, params)
+
+getUploadServer :: Config.BotType -> IO (Either Logger.LogMessage BSL.ByteString)  
+getUploadServer bot = do 
+    response <- runReq defaultHttpConfig $ do 
+        req 
+            POST 
+            (Wrapper.hostPathToUrlScheme VKData.uploadPhotoServerHostPath)
+            NoReqBody 
+            lbsResponse
+            (mconcat $ Wrapper.paramsToHttps <$> VKData.getUploadFileParams bot)
+    case responseStatusCode response of 
+        200 -> (pure . pure) (responseBody response :: BSL.ByteString) 
+        err -> pure $ Left (Logger.makeLogMessage LoggerMsgs.getUploadServerErr $ (T.pack . show) err)
+-}
+uploadServerHostPathParams :: Logger.Logger
+    ->  Either Logger.LogMessage VKStructs.VKUploadPhotoServerResponse
+    -> IO (Either Logger.LogMessage (PureStructs.HostPath, [PureStructs.Params])) 
+uploadServerHostPathParams _ (Left err) = pure $ Left err 
+uploadServerHostPathParams logger (Right (VKStructs.PhotoServerError _)) = pure $ Left LoggerMsgs.noHostPath 
+uploadServerHostPathParams logger (Right (VKStructs.PhotoServer (VKStructs.UploadUrl url))) = 
+    case urlToHostPath url of 
+        Nothing -> pure $ Left LoggerMsgs.noHostPath 
+        Just hostPath -> case urlToParams url of 
+            Nothing -> pure $ Left LoggerMsgs.noParams 
+            Just params -> pure $ Right (hostPath, params)
+
+makeVKUploadPhotoServerResponse :: Logger.Logger 
+    -> Either Logger.LogMessage BSL.ByteString 
+    -> IO (Either Logger.LogMessage VKStructs.VKUploadPhotoServerResponse)
+makeVKUploadPhotoServerResponse _ (Left err) = pure $ Left err
+makeVKUploadPhotoServerResponse logger (Right response) = do 
+    case eitherDecode response :: Either String VKStructs.VKUploadPhotoServerResponse of 
+        Left err -> pure $ Left (Logger.makeLogMessage LoggerMsgs.makeVKUploadPhotoServerResponseErr (T.pack err))
+        Right decoded -> case decoded of 
+            VKStructs.PhotoServer ps -> pure $ Right (VKStructs.PhotoServer ps)
+            VKStructs.PhotoServerError (VKStructs.VKResultError _ err_msg) -> 
+                pure $ Left (Logger.makeLogMessage LoggerMsgs.makeVKUploadPhotoServerResponseErr err_msg)
+
+
+getServerToUploadUrl :: Config.BotType 
+    -> Logger.Logger 
+    -> IO (Either Logger.LogMessage BSL.ByteString)  
+getServerToUploadUrl botType logger = do 
+    Logger.botLog logger LoggerMsgs.getServUpl
+    getResponseNoBody 
+        (hostPathToUrlScheme VKData.uploadPhotoServerHostPath)
+        (mconcat $ paramsToHttps <$> VKData.getUploadFileParams botType)
+    >>= responseToLbsByteString 
+
+downloadPhotoLbs :: T.Text -> IO (Either Logger.LogMessage BSL.ByteString)
+downloadPhotoLbs url = do 
+    http <- parseRequest $ T.unpack url 
+    updRequest <- httpLBS http
+    let respBody = getResponseBody updRequest 
+    pure $ Right respBody 
+            
+photoLbsToParam :: Either Logger.LogMessage BSL.ByteString
+    -> IO (Either Logger.LogMessage [PureStructs.Params])
+photoLbsToParam (Left err) = pure $ Left err 
+photoLbsToParam (Right photoLbs) = pure $ Right 
+    [PureStructs.ParamsFile "photo" photoLbs]
+
+-}
