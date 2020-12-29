358914deba0fddfd9d8021e1aecdcf603486ceb7
KateBu <mayflower.studio@gmail.com>
2020-12-29 17:23:42 +0300

    refactored

---
+++ src/API/Telegram/Cleaners/MbMsgType.hs
@@ -0,0 +1,49 @@
+module API.Telegram.Cleaners.MbMsgType (module MsgTypes) where
+
+import API.Telegram.Cleaners.MbTxt as MsgTypes
+    ( mbTextMessage, mkTxtMsg ) 
+import API.Telegram.Cleaners.MbAnimation as MsgTypes
+    ( mbAnimation, mbAnimation' ) 
+import API.Telegram.Cleaners.MbAudio as MsgTypes
+    ( mbAudio, mbAudio' ) 
+import API.Telegram.Cleaners.MbPoll as MsgTypes ( mbPoll, mbPoll' )
+import API.Telegram.Cleaners.MbDoc as MsgTypes ( mbDoc, mbDoc' ) 
+import API.Telegram.Cleaners.MbVideo as MsgTypes
+    ( mbVideo, mbVideo' ) 
+import API.Telegram.Cleaners.MbVoice as MsgTypes
+    ( mbVoice, mbVoice' ) 
+import API.Telegram.Cleaners.MbPhoto as MsgTypes
+    ( mbPhoto, getPhotoParams, mbPhoto' ) 
+import API.Telegram.Cleaners.MbContact as MsgTypes
+    ( mbContact, mbContact' ) 
+import API.Telegram.Cleaners.MbVenue as MsgTypes
+    ( mbVenue, mbVenue' ) 
+import API.Telegram.Cleaners.MbLocation as MsgTypes
+    ( mbLocation, mbLocation' ) 
+import API.Telegram.Cleaners.MbSticker as MsgTypes
+    ( mbSticker, mbSticker' ) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
