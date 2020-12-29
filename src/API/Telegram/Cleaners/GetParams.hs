94e0fe1f30526b207fa8d51b685fb0b42ca36b18
KateBu <mayflower.studio@gmail.com>
2020-12-29 16:49:57 +0300

    refactored

---
+++ src/API/Telegram/Cleaners/GetParams.hs
@@ -0,0 +1,16 @@
+module API.Telegram.Cleaners.GetParams where
+
+import qualified API.Telegram.Structs as TStructs 
+import qualified Logic.PureStructs as PureStructs 
+
+basicParams :: PureStructs.ChatID -> TStructs.MessageInfo -> [PureStructs.Params] 
+basicParams chid mInfo = PureStructs.ParamsNum "chat_id" chid : mbCaption mInfo 
+
+mbCaption :: TStructs.MessageInfo -> [PureStructs.Params]
+mbCaption mInfo = 
+    let mbCap = TStructs.caption mInfo
+    in maybe [] (pure . PureStructs.ParamsText "caption") mbCap 
+
+getPhotoParams :: [TStructs.TelPhoto] -> [PureStructs.Params]
+getPhotoParams [] = []
+getPhotoParams (x:_) = [PureStructs.ParamsText "photo" (TStructs.photo_file_id x)]
