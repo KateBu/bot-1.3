19d26f75f3bfaebe5e9e78b23e52a510327fda88
KateBu <mayflower.studio@gmail.com>
2020-12-31 16:16:45 +0300

    more data constructors added

--- src/Exceptions/Exceptions.hs
+++ src/Exceptions/Exceptions.hs
@@ -1,21 +1,30 @@
 module Exceptions.Exceptions where
 
 import qualified Data.Text as T
-import Control.Exception ( Exception, IOException ) 
+import Control.Exception ( SomeException, Exception, IOException ) 
 import qualified Logger.Logger as Logger 
 import qualified Logger.LoggerMsgs as LoggerMsgs
 
 data BotException = InitConfigExcept Logger.LogMessage 
     | IOExept IOException
     | ParseExcept Logger.LogMessage
+    | UpdateExcept Logger.LogMessage
+    | SendExcept Logger.LogMessage 
+    | OtherExcept SomeException 
 
 instance Show BotException where 
     show (InitConfigExcept logMsg) = 
         "InitConfigException occured -- " <> show logMsg
-    show (IOExept logMsg) =
-        "IO exception occured -- " <> show logMsg
+    show (IOExept ioEx) =
+        "IO exception occured -- " <> show ioEx
     show (ParseExcept logMsg) = 
-        "Parsing exceptions occured" <> show logMsg 
+        "Parsing exception occured -- " <> show logMsg 
+    show (UpdateExcept logMsg) = 
+        "Update exception occured -- " <> show logMsg 
+    show (SendExcept logMsg) = 
+        "Send exception occured -- " <> show logMsg 
+    show (OtherExcept oEx) = 
+        "Other exception occured -- " <> show oEx     
 
 instance Exception BotException 
 
@@ -32,6 +41,9 @@ throwInitConfigExcept = throwBotExcept $ InitConfigExcept LoggerMsgs.initConfigE
 
 throwParseExcept :: Monad m => String -> m (Either BotException a)
 throwParseExcept err = throwBotExcept $ ParseExcept (Logger.makeLogMessage LoggerMsgs.parseErr (T.pack err))
+
+throwUpdateExcept :: Monad m => Logger.LogMessage -> m (Either BotException a)
+throwUpdateExcept = pure . Left . UpdateExcept  
 
 catchBotExcept :: Monad m => (BotException -> Either BotException a)
     -> Either BotException a 
