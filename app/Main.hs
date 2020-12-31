d7593c65d556db13e96bfde11020a79c488c5a0c
KateBu <mayflower.studio@gmail.com>
2020-12-31 14:19:10 +0300

    main function changed

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,12 @@ module Main where
 
 import qualified API.Bot as Bot 
 import qualified Config.Config as Config 
-import qualified Exceptions.Exceptions as BotEx 
 
 configPath :: String 
 configPath = "config.config"
 
 main :: IO ()
-main = do
-    config <- Config.parseConfig configPath 
-    either BotEx.handleBotException Bot.runBot config 
+main = 
+    Config.parseConfig configPath >>= Bot.runBot  
     
  
