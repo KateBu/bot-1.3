a9364afd1344051eab2feb35d699aa13b4372a4d
KateBu <mayflower.studio@gmail.com>
2021-01-04 16:48:11 +0300

    new tests added

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,5 +3,20 @@ import Test.HUnit ( runTestTT )
 
 main :: IO ()
 main = do 
-    putStrLn " ...Running tests..."
+    putStrLn "Logic.getLastConf function tests:"
+    mapM_ runTestTT Tests.testGetLastConf
+    putStrLn "Logic.processMsgs function tests:"
+    mapM_ runTestTT Tests.testProcessMsgs
+    putStrLn "Logic.processMsgs_ function tests:"
+    mapM_ runTestTT Tests.testProcessMsgs_
+    putStrLn "Logic.processMsgsCallback function tests:"
+    mapM_ runTestTT Tests.testProcessMsgsCallback
+    putStrLn "Logic.processMsgsCommon function tests:"
+    mapM_ runTestTT Tests.testProcessMsgsCommon
+    putStrLn "Logic.makeRepeatMsg function tests:"
+    mapM_ runTestTT Tests.testMakeRepeatMsg
+    putStrLn "Logic.makeCallbackResponse function tests:"
+    runTestTT Tests.testMakeCallbackResponse 
+    putStrLn "Logic.repeatMsg function tests:"
     mapM_ runTestTT Tests.testsRepeatMsg
+    
