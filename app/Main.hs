bd68a750ac98afdbe09c725ce1fcc97757b7cbd2
KateBu <mayflower.studio@gmail.com>
2021-01-06 11:37:58 +0300

    formatted

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,11 @@
 module Main where
 
-import qualified API.Bot as Bot 
-import qualified Config.Config as Config 
+import qualified API.Bot as Bot
+import qualified Config.Config as Config
 
-configPath :: String 
+configPath :: String
 configPath = "config.config"
 
 main :: IO ()
-main = 
-    Config.parseConfig configPath >>= Bot.runBot'  
-    
- 
+main =
+  Config.parseConfig configPath >>= Bot.runBot
