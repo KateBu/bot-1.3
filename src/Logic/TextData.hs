6e56b998da2e9bf5a10ef5b70b526422d0a0216f
KateBu <mayflower.studio@gmail.com>
2021-01-06 15:21:33 +0300

    message language changed to English

--- src/Logic/TextData.hs
+++ src/Logic/TextData.hs
@@ -3,7 +3,7 @@ module Logic.TextData where
 import qualified Data.Text as T
 
 repeatText :: T.Text
-repeatText = "Выберите количество повторов: "
+repeatText = "Please, choose how many times you want to get your messages back: "
 
 newRepeatText :: Int -> T.Text
-newRepeatText rep = "Установлено количество повторов: " <> (T.pack . show) rep
+newRepeatText rep = "You chose: " <> (T.pack . show) rep
