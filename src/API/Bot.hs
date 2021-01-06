24be7a5f477594060f624b8200184d950bddb861
KateBu <mayflower.studio@gmail.com>
2021-01-06 11:38:29 +0300

    unnecessary functions removed

--- src/API/Bot.hs
+++ src/API/Bot.hs
@@ -7,7 +7,6 @@ import qualified Logger.Logger as Logger
 import qualified Logger.LoggerMsgs as LoggerMsgs 
 import qualified Logic.Logic as Logic 
 
-{-
 runBot :: Either BotEx.BotException Config.Config -> IO ()
 runBot (Left err) = BotEx.handleBotException err
 runBot (Right config) = do 
@@ -15,27 +14,10 @@ runBot (Right config) = do
     logger <- Handle.hLogger handle 
     conf <- Handle.hConfig handle 
     Handle.hGetUpdates handle conf logger 
-        >>= Logic.processMsgs config logger (Handle.hSendMessage_ handle)
-        >>= nextLoop logger 
--}
-runBot' :: Either BotEx.BotException Config.Config -> IO ()
-runBot' (Left err) = BotEx.handleBotException err
-runBot' (Right config) = do 
-    handle <- Handle.new config  
-    logger <- Handle.hLogger handle 
-    conf <- Handle.hConfig handle 
-    Handle.hGetUpdates handle conf logger 
         >>= Logic.processMsgs config logger (Handle.hSendMessage handle)
-        >>= nextLoop' logger 
-{-
-nextLoop :: Logger.Logger -> (Either Logger.LogMessage Config.Config) -> IO ()
-nextLoop logger (Left err) = do 
-    Logger.botLog logger (Logger.makeLogMessage err "\nProgram terminated")
-nextLoop logger (Right config) = do 
+        >>= nextLoop logger 
+
+nextLoop :: Logger.Logger -> (Either BotEx.BotException Config.Config) -> IO ()
+nextLoop logger config = do 
     Logger.botLog logger LoggerMsgs.nextLoop
-    runBot $ Right config  
--}
-nextLoop' :: Logger.Logger -> (Either BotEx.BotException Config.Config) -> IO ()
-nextLoop' logger config = do 
-    Logger.botLog logger LoggerMsgs.nextLoop
-    runBot' config 
+    runBot config 
