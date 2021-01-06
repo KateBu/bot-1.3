2c12b4f96d859fc87e761c4872ce7df10bfcce0f
KateBu <mayflower.studio@gmail.com>
2021-01-06 11:41:22 +0300

    imports changed

--- src/Config/Config.hs
+++ src/Config/Config.hs
@@ -3,9 +3,18 @@ module Config.Config (module Config) where
 import Config.ConfigStructs as Config
     ( Token, Users, VK(..), Telegram(..), BotType(..), Config(..) )
 import Config.ConfigData as Config
+    ( timeOut, vkApiVersion, vkLongPollUrl )
 import Config.MakeConfigFunctions as Config
-   
+    ( parseConfig )   
 import Config.ConfigFunctions as Config
+    ( addUser,
+      configGetUid,
+      configSetOffset,
+      deleteUser,
+      findUserRepeat,
+      reWriteUserRepeat,
+      setUserRepeat )
+    
     
 
 
