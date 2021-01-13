ad6fc77df48f72a414e0da057ee90b4f385d9bcc
KateBu <mayflower.studio@gmail.com>
2021-01-13 14:34:31 +0300

    path declaration removed

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,6 @@ module Main where
 import qualified API.Bot as Bot
 import qualified Config.Config as Config
 
-configPath :: String
-configPath = "config.config"
-
 main :: IO ()
 main =
-  Config.parseConfig configPath >>= Bot.runBot
+  Config.parseConfig' "config.config" >>= Bot.runBot'
