07b8c44ee35e655198d96b73a974b95befeb8990
KateBu <mayflower.studio@gmail.com>
2021-01-15 14:29:34 +0300

    formatted

--- src/Environment/EnvStructs.hs
+++ src/Environment/EnvStructs.hs
@@ -1,10 +1,9 @@
 module Environment.EnvStructs where
 
-import Config.Config ( Config) 
-import Logger.Logger ( Logger) 
+import Config.Config (Config)
+import Logger.Logger (Logger)
 
-data Env m = Env 
-  {
-    eConfig :: Config 
-    , eLogger :: Logger m
+data Env m = Env
+  { eConfig :: Config,
+    eLogger :: Logger m
   }
