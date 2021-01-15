5a67251b111b12eae5d4f31bcb130dcaa3ebddde
KateBu <mayflower.studio@gmail.com>
2021-01-15 14:28:59 +0300

    added functions that work with Config and Logger

--- src/Environment/EnvFunctions.hs
+++ src/Environment/EnvFunctions.hs
@@ -1,39 +1,68 @@
 module Environment.EnvFunctions where
 
-import Control.Monad.Reader
-    ( ReaderT, MonadIO(liftIO), MonadReader(ask), asks )
-import Environment.EnvStructs ( Env(..) )
-import qualified Config.Config as Config 
+import Control.Monad.State
+    ( MonadTrans(lift),
+      StateT(runStateT),
+      MonadState(put, get),
+      gets,
+      evalStateT,
+      execStateT )
+import Environment.EnvStructs (Env (..))
 import qualified Logger.Logger as Logger
+import qualified Config.Config as Config
 
-type RTEnvIO = ReaderT (Env IO) IO (Env IO)
-type RTEnvM m = ReaderT (Env m) m (Env m)
+type STEmvM m = StateT (Env m) m (Env m)
 
 eInitEnv :: Config.Config -> IO (Env IO)
-eInitEnv config = 
-    Env config <$> (Logger.createLogger $ Config.priority config)
+eInitEnv config =
+  Env config <$> Logger.createLogger (Config.priority config)
 
-eLog :: Logger.LogMessage ->  ReaderT (Env IO) IO ()
-eLog msg = do 
-    env <- ask 
-    liftIO $ Logger.botLog (eLogger env) msg 
+stLog :: Monad m => Logger.LogMessage -> StateT (Env m) m ()
+stLog msg = do
+  state <- get
+  lift $ Logger.botLog (eLogger state) msg
 
-eSetOffset :: (Monad m) => Int -> RTEnvM m
-eSetOffset newOffset = do 
-    env <- ask 
-    let newConfig = Config.configSetOffset (eConfig env) newOffset
-    pure $ env {eConfig = newConfig}
+eLog :: Monad m => Logger.LogMessage -> Env m -> m ()
+eLog msg env = do
+  runStateT (stLog msg) env
+  pure ()
 
-eGetConfig :: ReaderT (Env IO) IO Config.Config
-eGetConfig = asks eConfig
+stSetOffset :: (Monad m) => Int -> STEmvM m
+stSetOffset newOffset = do
+  state <- get
+  let newConfig = Config.configSetOffset (eConfig state) newOffset
+  put $ state {eConfig = newConfig}
+  pure state
 
-eSetUserRepeat :: (Monad m) => Int -> Int -> RTEnvM m 
-eSetUserRepeat chid newRep = do 
-    env <- ask 
-    pure $ env {eConfig = Config.setUserRepeat (eConfig env) chid newRep}
+eSetOffset :: (Monad m) => Int -> Env m -> m (Env m)
+eSetOffset newOffset = execStateT $ stSetOffset newOffset
 
-eFindUserRepeat :: (Monad m) => Int -> ReaderT (Env m) m Int 
-eFindUserRepeat chid = do 
-    env <- ask 
-    pure $ Config.findUserRepeat (eConfig env) chid 
+stFindUserRepeat :: (Monad m) => Int -> StateT (Env m) m Int
+stFindUserRepeat chid = do
+  state <- get
+  pure $ Config.findUserRepeat (eConfig state) chid
 
+eFindUserRepeat :: (Monad m) => Int -> Env m -> m Int
+eFindUserRepeat chid = evalStateT $ stFindUserRepeat chid
+
+stSetUserRepeat :: (Monad m) => Int -> Int -> STEmvM m
+stSetUserRepeat chid newRep = do
+  state <- get
+  put $ state {eConfig = Config.setUserRepeat (eConfig state) chid newRep}
+  pure state
+
+eSetUserRepeat :: Monad m => Int -> Int -> Env m -> m (Env m)
+eSetUserRepeat chid newRep = execStateT (stSetUserRepeat chid newRep)
+
+stGetConfig :: Monad m => StateT (Env m) m Config.Config
+stGetConfig = gets eConfig
+
+eGetConfig :: Monad m => Env m -> m Config.Config
+eGetConfig = evalStateT stGetConfig
+
+stGetBotType :: Monad m => StateT (Env m) m Config.BotType
+stGetBotType =
+  gets $ Config.botType . eConfig
+
+eGetBotType :: Monad m => Env m -> m Config.BotType
+eGetBotType = evalStateT stGetBotType
