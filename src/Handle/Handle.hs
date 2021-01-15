1ec221d96fddf0c23f5439f65f99f5d278154d72
KateBu <mayflower.studio@gmail.com>
2021-01-15 14:30:30 +0300

    removed unnecessary things

--- src/Handle/Handle.hs
+++ src/Handle/Handle.hs
@@ -2,44 +2,18 @@ module Handle.Handle where
 
 import qualified API.Wrapper as Wrapper
 import qualified Config.Config as Config
-import qualified Logger.Logger as Logger
+import qualified Environment.Environment as Env
 import qualified Logic.PureStructs as PureStructs
-import qualified Environment.Environment as Env 
-
-data Handle' m = Handle'
-  { hConfig' :: m Config.Config,
-  --  hLogger' :: m (Logger.Logger m),
-    hGetUpdates' ::
-      Env.Env m ->
-      m [PureStructs.PureMessage],
-    hSendMessage' ::
-      Env.Env m ->
-      PureStructs.PureMessage ->
-      m (Env.Env m)
-  }
-
-new' :: Config.Config -> IO (Handle' IO)
-new' config =
-  pure $
-    Handle'
-      { hConfig' = pure config,
-       -- hLogger' = Logger.createLogger (Config.priority config),
-        hGetUpdates' = Wrapper.getPureMessageList',
-        hSendMessage' = Wrapper.sendM' 
-      }
 
 data Handle m = Handle
   { hConfig :: m Config.Config,
-    hLogger :: m (Logger.Logger m),
     hGetUpdates ::
-      Config.Config ->
-      Logger.Logger m ->
+      Env.Env m ->
       m [PureStructs.PureMessage],
     hSendMessage ::
-      Config.Config ->
-      Logger.Logger m ->
+      Env.Env m ->
       PureStructs.PureMessage ->
-      m Config.Config
+      m (Env.Env m)
   }
 
 new :: Config.Config -> IO (Handle IO)
@@ -47,7 +21,6 @@ new config =
   pure $
     Handle
       { hConfig = pure config,
-        hLogger = Logger.createLogger (Config.priority config),
         hGetUpdates = Wrapper.getPureMessageList,
         hSendMessage = Wrapper.sendM
       }
