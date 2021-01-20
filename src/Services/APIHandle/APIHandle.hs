192323127a99ebbe0bd1adc6c5a6ce409804d259
KateBu <mayflower.studio@gmail.com>
2021-01-20 17:22:23 +0300

    API Handle is here now

---
+++ src/Services/APIHandle/APIHandle.hs
@@ -0,0 +1,23 @@
+module Services.APIHandle.APIHandle where
+
+import qualified API.Wrapper as Wrapper
+import qualified Environment.Environment as Env
+import qualified Logic.PureStructs as PureStructs
+
+data Handle m = Handle
+  { 
+    hGetUpdates ::
+      m [PureStructs.PureMessage],
+    hSendMessage ::
+      PureStructs.PureMessage ->
+      m (Env.Environment m)
+  }
+
+new :: Env.Environment IO -> IO (Handle IO)
+new env =
+  pure $
+    Handle
+      { 
+        hGetUpdates = Wrapper.getPureMessageList env,
+        hSendMessage = Wrapper.sendM env
+      }
