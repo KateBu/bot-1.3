e388b3be6f94f2b83b4505b5893cc2f3289c9aa8
KateBu <mayflower.studio@gmail.com>
2021-01-20 17:18:20 +0300

    main function changed

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,8 @@
 module Main where
 
 import qualified API.Bot as Bot
-import qualified Config.Config as Config
 import qualified Environment.Environment as Env 
 
 main :: IO ()
-main =
-  Config.parseConfig "config.config" >>= Env.eInitEnv >>= Bot.runBot
+main = 
+  Env.setEnvironment "config.config" >>= Bot.runBot
