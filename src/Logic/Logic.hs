6a03bd46cbd3b9b14fcc54f25c1e197f0fe22b3c
KateBu <mayflower.studio@gmail.com>
2021-01-22 09:34:03 +0300

    processMsgs fixed

--- src/Logic/Logic.hs
+++ src/Logic/Logic.hs
@@ -1,6 +1,6 @@
 module Logic.Logic where
 
-import Control.Monad.Reader
+import Control.Monad.Reader ( ReaderT(runReaderT) )
 import qualified Environment.Environment as Env
 import qualified Environment.Logger.Logger as Logger
 import qualified Environment.Logger.LoggerMsgs as LoggerMsgs
@@ -16,8 +16,10 @@ processMsgs ::
   Handle.SHandle m ->
   [PureStructs.PureMessage] ->
   m (Env.Environment m)
-processMsgs env handle msgs =
-  mapM (processMsgs_ env handle) msgs >>= getLast env
+processMsgs env _ [] = pure env 
+processMsgs env handle (x:xs) = do 
+  env' <- processMsgs_ env handle x 
+  processMsgs env' handle xs 
 
 processMsgs_ ::
   (Monad m) =>
@@ -48,10 +50,6 @@ processMsgs_ env handle msg = do
 processMsgsErr :: Monad m => m (Env.Environment m)
 processMsgsErr = BotEx.throwOtherException LoggerMsgs.chidNotFound
 
-getLast :: Monad m => Env.Environment m -> [Env.Environment m] -> m (Env.Environment m)
-getLast env [] = pure env
-getLast _ envs = pure . last $ envs
-
 makeRepeatMsg :: PureStructs.PureMessage -> PureStructs.PureMessage
 makeRepeatMsg msg =
   PureStructs.PureMessage
@@ -59,46 +57,3 @@ makeRepeatMsg msg =
     (PureStructs.updateID msg)
     (PureStructs.mbChatID msg)
     (mconcat [PureStructs.mbParams msg, Just [PureStructs.ParamsText "text" PureStructs.repeatText]])
-
-{-
-processMsgs ::
-  (Monad m) =>
-  Env.Environment m ->
-  PureStructs.SendFunction m ->
-  [PureStructs.PureMessage] ->
-  m (Env.Environment m)
-processMsgs env sendFunction msgs =
-  mapM (processMsgs_ env sendFunction) msgs >>= getLast env
-
-processMsgs_ ::
-  (Monad m) =>
-  Env.Environment m ->
-  PureStructs.SendFunction m ->
-  PureStructs.PureMessage ->
-  m (Env.Environment m)
-processMsgs_ env sendFunction msg = case PureStructs.messageType msg of
-  PureStructs.MTEmpty -> undefined --Env.eSetOffset ((succ . PureStructs.updateID) msg) env
-  PureStructs.MTUserCommand PureStructs.Help -> sendFunction env msg
-  PureStructs.MTUserCommand PureStructs.Repeat -> sendFunction env (makeRepeatMsg msg)
-  PureStructs.MTCallbackQuery callbackData -> do
-    let mbChid = PureStructs.mbChatID msg
-    maybe processMsgsErr (processMsgsCallback env sendFunction msg callbackData) mbChid
-  PureStructs.MTCommon _ -> do
-    let mbChid = PureStructs.mbChatID msg
-    maybe processMsgsErr (processMsgsCommon env sendFunction msg) mbChid
-
-processMsgsErr :: Monad m => m (Env.Environment m)
-processMsgsErr = BotEx.throwOtherException LoggerMsgs.chidNotFound
-
-getLast :: Monad m => Env.Environment m -> [Env.Environment m] -> m (Env.Environment m)
-getLast env [] = pure env
-getLast _ envs = pure . last $ envs
-
-makeRepeatMsg :: PureStructs.PureMessage -> PureStructs.PureMessage
-makeRepeatMsg msg =
-  PureStructs.PureMessage
-    (PureStructs.MTCommon "Message")
-    (PureStructs.updateID msg)
-    (PureStructs.mbChatID msg)
-    (mconcat [PureStructs.mbParams msg, Just [PureStructs.ParamsText "text" PureStructs.repeatText]])
--}
