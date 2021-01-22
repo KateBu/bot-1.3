d01147ba949aa0804f4f12a6f50fb20293522518
KateBu <mayflower.studio@gmail.com>
2021-01-22 11:14:06 +0300

    imports listed

--- src/Environment/Environment.hs
+++ src/Environment/Environment.hs
@@ -1,7 +1,15 @@
 module Environment.Environment (module Env) where
 
 import Environment.EnvFunctions as Env
-import Environment.EnvStructs as Env
+    ( eConfig,
+      eGetUid,
+      eHelpMsg,
+      eLogger,
+      eRep,
+      eSetOffset,
+      updateConfig,
+      REnv )
+import Environment.EnvStructs as Env ( Environment(..) )
 import Environment.InitEnvironment as Env
   ( setEnvironment,
   )
