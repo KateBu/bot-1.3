658a1586ae42ce80f813182e9d7680ec91d42211
KateBu <mayflower.studio@gmail.com>
2021-01-28 11:24:09 +0300

    some tests added

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,34 +3,30 @@ import Tests as Tests
 
 
 main :: IO ()
-main = pure ()
- {- do
-  putStrLn "Config.setUserRepeat function tests:"
-  mapM_ runTestTT Tests.testSetUserRepeat
-  putStrLn "Config.reWriteUserRepeat function tests:"
-  mapM_ runTestTT Tests.testReWriteUserRepeat
-  putStrLn "Config.findUserRepeat function tests:"
-  mapM_ runTestTT Tests.testFindUserRepeat
-  putStrLn "Config.deleteUser function tests:"
-  mapM_ runTestTT Tests.testDeleteUser
-  putStrLn "Config.addUser function tests:"
-  mapM_ runTestTT Tests.testAddUser
-  putStrLn "Config.configSetOffset function tests:"
-  mapM_ runTestTT Tests.testConfigSetOffset
-  putStrLn "Logic.getLastConf function tests:"
-  mapM_ runTestTT Tests.testGetLastConf
+main = do
+    putStrLn "Config.configSetOffset function tests:"
+    mapM_ runTestTT Tests.testConfigSetOffset
+    putStrLn "Config.configGetUid function tests:"
+    mapM_ runTestTT Tests.testConfigGetUid 
+    putStrLn "Logic.ProcMsgs.Callback.makeCallbackResponse function tests:"
+    runTestTT Tests.testMakeCallbackResponse
+    putStrLn "Logic.makeRepeatMsg function tests:"
+    mapM_ runTestTT Tests.testMakeRepeatMsg
+    putStrLn "Logic.ProcMsgs.Common.repeatMsg function tests:"
+    mapM_ runTestTT Tests.testsRepeatMsg
+    putStrLn "Logic.ProcMsgs.Common.processMsgsCommon function tests:"
+    mapM_ runTestTT Tests.testProcessMsgsCommon
+  {-
+  
+  
   putStrLn "Logic.processMsgs function tests:"
   mapM_ runTestTT Tests.testProcessMsgs
   putStrLn "Logic.processMsgs_ function tests:"
   mapM_ runTestTT Tests.testProcessMsgs_
   putStrLn "Logic.processMsgsCallback function tests:"
   mapM_ runTestTT Tests.testProcessMsgsCallback
-  putStrLn "Logic.processMsgsCommon function tests:"
-  mapM_ runTestTT Tests.testProcessMsgsCommon
-  putStrLn "Logic.makeRepeatMsg function tests:"
-  mapM_ runTestTT Tests.testMakeRepeatMsg
-  putStrLn "Logic.makeCallbackResponse function tests:"
-  runTestTT Tests.testMakeCallbackResponse
-  putStrLn "Logic.repeatMsg function tests:"
-  mapM_ runTestTT Tests.testsRepeatMsg
+
+  
+  
+  
 -}
