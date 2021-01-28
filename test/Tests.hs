91472a63471f0abd588fc1b8f8b93e85dfd43a0b
KateBu <mayflower.studio@gmail.com>
2021-01-28 11:23:12 +0300

    qualified imports added

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,14 +1,19 @@
 module Tests (module Tests) where
 
-
-import Tests.ConfigSetOffset as Tests
-    ( testConfigSetOffset, )
 import Tests.ConfigGetUid as Tests
-    ( testConfigGetUid,) 
+  ( testConfigGetUid,
+  )
+import Tests.ConfigSetOffset as Tests
+  ( testConfigSetOffset,
+  )
 import Tests.MakeCallbackResponse as Tests
 import Tests.MakeRepeatMsg as Tests
 import Tests.ProcessMsgs as Tests
 import Tests.ProcessMsgsCallback as Tests
 import Tests.ProcessMsgsCommon as Tests
+  ( testProcessMsgsCommon,
+  )
 import Tests.ProcessMsgs_ as Tests
 import Tests.RepeatMsgs as Tests
+  ( testsRepeatMsg,
+  )
