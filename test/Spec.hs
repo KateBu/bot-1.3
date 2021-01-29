a0c3c80da9d7f1accc878dcf4ab0eb73e6c11b72
KateBu <mayflower.studio@gmail.com>
2021-01-29 10:45:29 +0300

    all the tests rewritten

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,32 +1,23 @@
 import Test.HUnit (runTestTT)
 import Tests as Tests
 
-
 main :: IO ()
 main = do
-    putStrLn "Config.configSetOffset function tests:"
-    mapM_ runTestTT Tests.testConfigSetOffset
-    putStrLn "Config.configGetUid function tests:"
-    mapM_ runTestTT Tests.testConfigGetUid 
-    putStrLn "Logic.ProcMsgs.Callback.makeCallbackResponse function tests:"
-    runTestTT Tests.testMakeCallbackResponse
-    putStrLn "Logic.makeRepeatMsg function tests:"
-    mapM_ runTestTT Tests.testMakeRepeatMsg
-    putStrLn "Logic.ProcMsgs.Common.repeatMsg function tests:"
-    mapM_ runTestTT Tests.testsRepeatMsg
-    putStrLn "Logic.ProcMsgs.Common.processMsgsCommon function tests:"
-    mapM_ runTestTT Tests.testProcessMsgsCommon
-  {-
-  
-  
-  putStrLn "Logic.processMsgs function tests:"
-  mapM_ runTestTT Tests.testProcessMsgs
+  putStrLn "Config.configSetOffset function tests:"
+  mapM_ runTestTT Tests.testConfigSetOffset
+  putStrLn "Config.configGetUid function tests:"
+  mapM_ runTestTT Tests.testConfigGetUid
+  putStrLn "Logic.ProcMsgs.Callback.makeCallbackResponse function tests:"
+  runTestTT Tests.testMakeCallbackResponse
+  putStrLn "Logic.makeRepeatMsg function tests:"
+  mapM_ runTestTT Tests.testMakeRepeatMsg
+  putStrLn "Logic.ProcMsgs.Common.repeatMsg function tests:"
+  mapM_ runTestTT Tests.testsRepeatMsg
+  putStrLn "Logic.ProcMsgs.Common.processMsgsCommon function tests:"
+  mapM_ runTestTT Tests.testProcessMsgsCommon
+  putStrLn "Logic.ProcMsgs.Callback.processMsgsCallback function tests:"
+  mapM_ runTestTT Tests.testProcessMsgsCallback
   putStrLn "Logic.processMsgs_ function tests:"
   mapM_ runTestTT Tests.testProcessMsgs_
-  putStrLn "Logic.processMsgsCallback function tests:"
-  mapM_ runTestTT Tests.testProcessMsgsCallback
-
-  
-  
-  
--}
+  putStrLn "Logic.processMsgs function tests:"
+  mapM_ runTestTT Tests.testProcessMsgs
