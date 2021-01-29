a1aa96d3ab29a4c32742277c7f5489ab1c1f8bcf
KateBu <mayflower.studio@gmail.com>
2021-01-29 10:45:57 +0300

    qualified imports added

--- test/Tests.hs
+++ test/Tests.hs
@@ -7,13 +7,23 @@ import Tests.ConfigSetOffset as Tests
   ( testConfigSetOffset,
   )
 import Tests.MakeCallbackResponse as Tests
+  ( testMakeCallbackResponse,
+  )
 import Tests.MakeRepeatMsg as Tests
+  ( testMakeRepeatMsg,
+  )
 import Tests.ProcessMsgs as Tests
+  ( testProcessMsgs,
+  )
 import Tests.ProcessMsgsCallback as Tests
+  ( testProcessMsgsCallback,
+  )
 import Tests.ProcessMsgsCommon as Tests
   ( testProcessMsgsCommon,
   )
 import Tests.ProcessMsgs_ as Tests
+  ( testProcessMsgs_,
+  )
 import Tests.RepeatMsgs as Tests
   ( testsRepeatMsg,
   )
