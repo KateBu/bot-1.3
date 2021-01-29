c0c9fff19e2872845128776997be07d56c7bb06d
KateBu <mayflower.studio@gmail.com>
2021-01-29 10:46:42 +0300

    tests rewritten

--- test/Tests/ProcessMsgsCallback.hs
+++ test/Tests/ProcessMsgsCallback.hs
@@ -1,20 +1,16 @@
 module Tests.ProcessMsgsCallback where
 
 import qualified Config.Config as Config
-import qualified Exceptions.Exceptions as BotEx
-import qualified Logic.Logic as Logic
+import qualified Logic.ProcMsgs.Callback as Logic
+import qualified Environment.Environment as Env
 import qualified Logic.PureStructs as PureStructs
 import Test.HUnit (Test (TestCase), assertEqual)
 import qualified TestData as TestData
 
-{-
+
 testProcessMsgsCallback :: [Test]
 testProcessMsgsCallback =
-  [ testProcessMsgsCallback1,
-    testProcessMsgsCallback2,
-    testProcessMsgsCallback3,
-    testProcessMsgsCallback4
-  ]
+  [ testProcessMsgsCallback1 ]
 
 testProcessMsgsCallback1 :: Test
 testProcessMsgsCallback1 =
@@ -25,90 +21,13 @@ testProcessMsgsCallback1 =
         actualProcessMsgsCallback1
     )
 
-actualProcessMsgsCallback1 :: Maybe (Either BotEx.BotException Config.Config)
-actualProcessMsgsCallback1 =
-  Logic.processMsgsCallback
-    TestData.testConfigTelegram
-    5
-    TestData.testFunction1
-    TestData.cbMsg2
-    PureStructs.rep5
-    11
+actualProcessMsgsCallback1 :: Maybe Config.Config
+actualProcessMsgsCallback1 = Env.config
+    <$> Logic.processMsgsCallback
+      TestData.servicesTel1
+      TestData.cbMsg2
+      PureStructs.rep5
+      11
 
-expectedProcessMsgsCallback1 :: Maybe (Either BotEx.BotException Config.Config)
-expectedProcessMsgsCallback1 = pure . pure $ Config.configSetOffset newConfig 1
-  where
-    newConfig = Config.setUserRepeat TestData.testConfigTelegram 11 5
-
-testProcessMsgsCallback2 :: Test
-testProcessMsgsCallback2 =
-  TestCase
-    ( assertEqual
-        "Logic.testProcessMsgsCallback2"
-        expectedProcessMsgsCallback2
-        actualProcessMsgsCallback2
-    )
-
-actualProcessMsgsCallback2 :: Maybe (Either BotEx.BotException Config.Config)
-actualProcessMsgsCallback2 =
-  Logic.processMsgsCallback
-    TestData.testConfigTelegram
-    3
-    TestData.testFunction1
-    TestData.cbMsg5
-    PureStructs.rep3
-    42
-
-expectedProcessMsgsCallback2 :: Maybe (Either BotEx.BotException Config.Config)
-expectedProcessMsgsCallback2 = pure . pure $ Config.configSetOffset newConfig 1
-  where
-    newConfig = Config.setUserRepeat TestData.testConfigTelegram 42 3
-
-testProcessMsgsCallback3 :: Test
-testProcessMsgsCallback3 =
-  TestCase
-    ( assertEqual
-        "Logic.testProcessMsgsCallback3"
-        expectedProcessMsgsCallback3
-        actualProcessMsgsCallback3
-    )
-
-actualProcessMsgsCallback3 :: Maybe (Either BotEx.BotException Config.Config)
-actualProcessMsgsCallback3 =
-  Logic.processMsgsCallback
-    TestData.testConfigVK
-    3
-    TestData.testFunction1
-    TestData.cbMsg5
-    PureStructs.rep2
-    42
-
-expectedProcessMsgsCallback3 :: Maybe (Either BotEx.BotException Config.Config)
-expectedProcessMsgsCallback3 = pure . pure $ Config.configSetOffset newConfig 1
-  where
-    newConfig = Config.setUserRepeat TestData.testConfigVK 42 2
-
-testProcessMsgsCallback4 :: Test
-testProcessMsgsCallback4 =
-  TestCase
-    ( assertEqual
-        "Logic.testProcessMsgsCallback3"
-        expectedProcessMsgsCallback4
-        actualProcessMsgsCallback4
-    )
-
-actualProcessMsgsCallback4 :: Maybe (Either BotEx.BotException Config.Config)
-actualProcessMsgsCallback4 =
-  Logic.processMsgsCallback
-    TestData.testConfigVK
-    3
-    TestData.testFunction1
-    TestData.cbMsg5
-    "PureStructs.rep1"
-    22
-
-expectedProcessMsgsCallback4 :: Maybe (Either BotEx.BotException Config.Config)
-expectedProcessMsgsCallback4 = pure . pure $ Config.configSetOffset newConfig 1
-  where
-    newConfig = Config.setUserRepeat TestData.testConfigVK 22 5
--}
+expectedProcessMsgsCallback1 :: Maybe Config.Config
+expectedProcessMsgsCallback1 =  Env.config <$> Env.eSetOffset TestData.testEnvTelegram 3
