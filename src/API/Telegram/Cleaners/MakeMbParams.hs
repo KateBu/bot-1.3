f7dd7348291e22e98302d11df94146bab4ab6b77
KateBu <mayflower.studio@gmail.com>
2021-02-02 14:12:52 +0300

    aliases added

--- src/API/Telegram/Cleaners/MakeMbParams.hs
+++ src/API/Telegram/Cleaners/MakeMbParams.hs
@@ -3,14 +3,22 @@ module API.Telegram.Cleaners.MakeMbParams where
 import qualified Data.Text as T
 import qualified Logic.PureStructs as PureStructs
 
-makeMaybeTextParams :: T.Text -> Maybe T.Text -> [PureStructs.Params]
+type Key = T.Text
+
+type TextValue = T.Text
+
+type BoolValue = Bool
+
+type IntValue = Int
+
+makeMaybeTextParams :: Key -> Maybe TextValue -> [PureStructs.Params]
 makeMaybeTextParams _ Nothing = []
 makeMaybeTextParams key (Just val) = pure $ PureStructs.ParamsText key val
 
-makeMaybeBoolParams :: T.Text -> Maybe Bool -> [PureStructs.Params]
+makeMaybeBoolParams :: Key -> Maybe BoolValue -> [PureStructs.Params]
 makeMaybeBoolParams _ Nothing = []
 makeMaybeBoolParams key (Just val) = pure $ PureStructs.ParamsBool key val
 
-makeMaybeNumParams :: T.Text -> Maybe Int -> [PureStructs.Params]
+makeMaybeNumParams :: Key -> Maybe IntValue -> [PureStructs.Params]
 makeMaybeNumParams _ Nothing = []
 makeMaybeNumParams key (Just val) = pure $ PureStructs.ParamsNum key val
