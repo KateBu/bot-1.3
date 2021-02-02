c3168990555762dde6960ee7176d70d41914454a
KateBu <mayflower.studio@gmail.com>
2021-02-02 14:35:38 +0300

    fixed

--- src/API/Telegram/Cleaners/MakeMbParams.hs
+++ src/API/Telegram/Cleaners/MakeMbParams.hs
@@ -5,20 +5,14 @@ import qualified Logic.PureStructs as PureStructs
 
 type Key = T.Text
 
-type TextValue = T.Text
-
-type BoolValue = Bool
-
-type IntValue = Int
-
-makeMaybeTextParams :: Key -> Maybe TextValue -> [PureStructs.Params]
+makeMaybeTextParams :: Key -> Maybe T.Text -> [PureStructs.Params]
 makeMaybeTextParams _ Nothing = []
 makeMaybeTextParams key (Just val) = pure $ PureStructs.ParamsText key val
 
-makeMaybeBoolParams :: Key -> Maybe BoolValue -> [PureStructs.Params]
+makeMaybeBoolParams :: Key -> Maybe Bool -> [PureStructs.Params]
 makeMaybeBoolParams _ Nothing = []
 makeMaybeBoolParams key (Just val) = pure $ PureStructs.ParamsBool key val
 
-makeMaybeNumParams :: Key -> Maybe IntValue -> [PureStructs.Params]
+makeMaybeNumParams :: Key -> Maybe Int -> [PureStructs.Params]
 makeMaybeNumParams _ Nothing = []
 makeMaybeNumParams key (Just val) = pure $ PureStructs.ParamsNum key val
