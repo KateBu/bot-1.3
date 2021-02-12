b8c49a7675139dee716c5c8b5e838e0c4f3f9fbd
KateBu <mayflower.studio@gmail.com>
2021-02-12 09:18:28 +0300

    removed import

--- src/TextMessages/RepeatCommandMessages.hs
+++ src/TextMessages/RepeatCommandMessages.hs
@@ -1,10 +1,9 @@
 module TextMessages.RepeatCommandMessages where
 
 import qualified Data.Text as T
-import qualified Environment.Exports as Env
 
 repeatText :: T.Text
 repeatText = "Please, choose how many times you want to get your messages back: "
 
-newRepeatText :: Env.RepeatNumber -> T.Text
+newRepeatText :: Int -> T.Text
 newRepeatText repeatNumber = "You chose: " <> (T.pack . show) repeatNumber
